06036620e25e76a3e91fac06790a0760299ed1b2
漩涡 <vars.cn@gmail.com>
2014-05-29 00:04:06 +0800

    测试VS2013

---
+++ OpenJudge/dsalgo/2-1.cpp
@@ -0,0 +1,25 @@
+#include  <iostream>
+
+using namespace std;
+
+struct LinkNode {
+    int data;
+    LinkNode * link;
+};
+
+class List {
+private:
+    LinkNode * first;
+public:
+    bool Insert(int i, int &x);
+
+
+};
+
+bool List::Insert(int i, int &x) {
+    if(first == NULL || i==0) {
+        LinkNode * newNode = new LinkNode(x);
+        if(newNode == NULL) {cerr<<"存储分配错误\n"<<endl}
+    }
+}
+int main()
