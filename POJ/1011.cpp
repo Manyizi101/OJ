71e35eb750b08a690d21c8f5cce5299e1efaf841
Xuanwo <vars.cn@gmail.com>
2014-07-16 12:11:38 +0800

    搜索+剪枝

--- POJ/1011.cpp
+++ POJ/1011.cpp
@@ -1,29 +1,82 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int n;
-int stick[100];
-int sum, big;
-int i;
-int ans;
+int sticks[64], n, len, num;
+bool used[64];
+
+bool compare(int a, int b)
+{
+    return a > b;
+}
+
+bool dfs(int cur, int left, int level)
+{
+    if (left == 0)
+    {
+        if (level == num - 2)
+            return true;
+        for (cur = 0; used[cur]; cur++)
+            ;
+        used[cur] = true;
+        if (dfs(cur + 1, len - sticks[cur], level + 1))
+            return true;
+        used[cur] = false;
+        return false;
+    }
+    else
+    {
+        if (cur >= n - 1)
+            return false;
+        for (int i = cur; i < n; i++)
+        {
+            if (used[i])
+                continue;
+            if ((sticks[i] == sticks[i - 1]) && !used[i - 1])
+                continue;
+            if (sticks[i] > left)
+                continue;
+            used[i] = true;
+            if (dfs(i, left - sticks[i], level))
+                return true;
+            used[i] = false;
+        }
+        return false;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-    while (cin >> n && n != 0)
+    while (cin >> n)
     {
-        sum = big = 0;
-        for (i = 0; i < n; i++)
+        if (n == 0)
+            break;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
         {
-            cin >> stick[i];
-            sum += stick[i];
-            if (stick[i] > big)    big = stick[i];
+            scanf("%d", &sticks[i]);
+            sum += sticks[i];
         }
-        for (i = 1; i <= n; i++)
+        sort(sticks, sticks + n, compare);
+        bool end = false;
+        for (len = sticks[0]; len <= sum / 2; len++)
         {
-            if (sum % i == 0 && sum / i >= big)    ans = i;
+            if (sum % len == 0)
+            {
+                used[0] = true;
+                num = sum / len;
+                if (dfs(0, len - sticks[0], 0))
+                {
+                    end = true;
+                    printf("%d\n", len);
+                    break;
+                }
+                used[0] = false;
+            }
         }
-        cout << sum / ans << endl;
+        if (!end)
+            printf("%d\n", sum);
+        memset(used, 0, sizeof(used));
     }
     return 0;
 }
