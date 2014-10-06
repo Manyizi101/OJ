1e2df26d71766e7b7827083c9664623d43bc858a
Xuanwo <vars.cn@gmail.com>
2014-10-07 00:27:48 +0800

    新增 POJ 2479

--- POJ/2479.cpp
+++ POJ/2479.cpp
@@ -13,7 +13,6 @@
 #include <map>
 #include <stack>
 #include <queue>
-#include <numeric>
 #include <iomanip>
 #include <bitset>
 #include <sstream>
@@ -24,26 +23,65 @@
 #define inf (1<<28)
 using namespace std;
 
+#define LOCAL
 #define MAXN 50001
 
-int t, n, a[MAXN];
-int left;
+int t, a[MAXN];
+int l[MAXN], r[MAXN], dp[MAXN], ans, big;
+int  m, i, res, tmp;
+
 void init()
 {
+    ans = 0, big = 0;
     memset(a, 0, sizeof(a));
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)    scanf("%d", &a[i]);
+    memset(l, 0, sizeof(l));
+    memset(r, 0, sizeof(r));
+    memset(dp, 0, sizeof(dp));
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++)    scanf("%d", &a[i]);
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
+#ifdef LOCAL
     freopen("2479.in", "r", stdin);
     freopen("2479.out", "w", stdout);
-    scanf(" % d", &t);
+#endif
+    scanf("%d", &t);
     while (t--)
     {
         init();
+        tmp = 0;
+        res = -10001;
+        for (i = 0; i < m; ++i)
+        {
+            tmp += a[i];
+            if (tmp > res)
+                res = tmp;
+            if (tmp < 0)
+                tmp = 0;
+            l[i] = res;
+        }
+        res = -10001;
+        tmp = 0;
+        for (i = m - 1; i >= 0; --i)
+        {
+            tmp += a[i];
+            if (tmp > res)
+                res = tmp;
+            if (tmp < 0)
+                tmp = 0;
+            r[i] = res;
+        }
+        res = l[0] + r[1];
+        for (i = 0; i < m - 1; ++i)
+        {
+            tmp = l[i] + r[i + 1];
+            if (tmp > res)
+                res = tmp;
+        }
+        printf ("%d\n", res);
     }
-    cout<<"Hello,world!!!!";
+
     return 0;
 }
