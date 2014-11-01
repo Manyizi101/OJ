2c9789fab7ff4252ac69677f199a5d45ba59ac1b
Xuanwo <vars.cn@gmail.com>
2014-11-01 10:32:33 +0800

    新增 CodeVS 1212

--- CodeVS/1012.cpp
+++ CodeVS/1012.cpp
@@ -18,15 +18,38 @@
 #include <bitset>
 #include <sstream>
 #include <fstream>
-#define debug puts("-----")
+#define debug "output for debug\n"
 #define pi (acos(-1.0))
 #define eps (1e-8)
 #define inf (1<<28)
 #define ll long long int
 using namespace std;
 
+int x,y;
+int ans=0,flag=0;
+
+int gcd(int a,int b)
+{
+    return b==0?a:gcd(b,a%b);
+}
+
 int main(int argc, char const *argv[])
 {
-	/* code */
-	return 0;
+    while(~scanf("%d%d", &x,&y))
+    {
+        if(x>y) swap(x,y);
+        ans=0;
+        for(int i=x; i<=y; i+=x)
+        {
+            for(int j=i; j<=y; j+=x)
+            {
+                if(i*j/x==y&&gcd(i,j)==x)
+                {
+                    ans++;
+                }
+            }
+        }
+        printf("%d\n", ans<<1);
+    }
+    return 0;
 }
