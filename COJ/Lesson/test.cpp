b59d82d39964d47656bbf26f2359316f7be2202a
Xuanwo <xuanwo.cn@gmail.com>
2015-03-18 21:01:24 +0800

    测试文件

--- COJ/Lesson/test.cpp
+++ COJ/Lesson/test.cpp
@@ -1,45 +1,11 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <ctime>
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <deque>
-#include <list>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <numeric>
-#include <iomanip>
-#include <bitset>
-#include <sstream>
-#include <fstream>
-#define debug puts("-----")
-#define pi (acos(-1.0))
-#define eps (1e-8)
-#define inf (1<<28)
-#define ll long long int
-using namespace std;
-
-int t;
-int n;
-int tmp;
-
-int main(int argc, char const *argv[])
+bool IsPrime(unsigned n)  //此处n是一个大于2的整数
 {
-	scanf("%d", &t);
-	while(t--)
+	for (unsigned i = 2; i < n / 2 + 1; ++i)
 	{
-		n=0;
-		while(scanf("%1d", &tmp))
+		if (n % i == 0)
 		{
-			n+=tmp;
+			return false;
 		}
-		n%3==0?puts("YES"):puts("NO");
 	}
-	return 0;
+	return true;
 }
