b3357d30a72972961149bc12a600d5d1beecdc2c
Xuanwo <xuanwo.cn@gmail.com>
2015-03-29 12:58:30 +0800

    太傻逼了- -，B题卡这么久

--- VJudge/73204A.cpp
+++ VJudge/73204A.cpp
@@ -25,14 +25,35 @@ const double eps = (1e-8);
 const int inf = 1<<31;
 using namespace std;
 
-const int MAXN = ;
+const int MAXN = 30;
+
+string a[MAXN]= {"ABSINTH", "BEER", "BRANDY", "CHAMPAGNE", "GIN", "RUM", "SAKE", "TEQUILA", "VODKA", "WHISKEY", "WINE","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","0"};
+int ans=0;
+int n;
+string x;
+
+bool check(string x)
+{
+	for(int i=0;i<29;i++)
+	{
+		if(!x.compare(a[i])) return true;
+	}
+	return false;
+}
 
 void init()
 {
+	scanf("%d", &n);
 }
 
 int main(int argc, char const *argv[])
 {
 	init();
+	for(int i=0;i<n;i++)
+	{
+		cin>>x;
+		if(check(x))	ans++;
+	}
+	cout<<ans;
 	return 0;
 }
--- VJudge/73204B.cpp
+++ VJudge/73204B.cpp
@@ -22,17 +22,82 @@
 #define ll long long int
 const double pi = acos(-1.0);
 const double eps = (1e-8);
-const int inf = 1<<31;
+const int inf = 1 << 20;
 using namespace std;
 
-const int MAXN = ;
+char x;
+int a1;
+ll ans;
+ll a[4];
 
 void init()
 {
+	memset(a, 0, sizeof(a));
+	a1 = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> x;
+		if (x == '*')	a1++;
+	}
+	sort(a, a + 4);
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char const * argv[])
 {
 	init();
+	if (a1 == 3)
+	{
+		ans = 1;
+		for (int i = 0; i < 4; i++)
+		{
+			ans *= a[i];
+		}
+	}
+	else if (a1 == 2)
+	{
+		ans = inf;
+		ll tmp;
+		for (int i = 0; i < 4; i++)
+		{
+			tmp = a[(i + 0) % 4] * a[(i + 1) % 4] * a[(i + 2) % 4] + a[(i + 3) % 4];
+		}
+		ans=min(ans,tmp);
+		for (int i = 0; i < 4; i++)
+		{
+			tmp = a[(i + 0) % 4] * a[(i + 1) % 4] + a[(i + 2) % 4] * a[(i + 3) % 4];
+		}
+		ans=min(ans,tmp);
+		tmp = a[0]*a[2]+a[1]*a[3];
+		ans=min(ans,tmp);
+		tmp = a[1]*a[3]+a[0]*a[2];
+		ans=min(ans,tmp);
+	}
+	else if(a1==1)
+	{
+		ans = inf;
+		ll tmp;
+		for (int i = 0; i < 4; i++)
+		{
+			tmp = a[(i + 0) % 4] * a[(i + 1) % 4] + a[(i + 2) % 4] + a[(i + 3) % 4];
+		}
+		ans=min(ans,tmp);
+		tmp = a[0]*a[2]+a[1]+a[3];
+		ans=min(ans,tmp);
+		tmp = a[1]*a[3]+a[0]+a[2];
+		ans=min(ans,tmp);
+	}
+	else
+	{
+		ans = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			ans += a[i];
+		}
+	}
+	cout << ans;
 	return 0;
 }
