de4ce18eb85c8b3bc763191838bd2db642ad0a2b
Xuanwo <xuanwo.cn@gmail.com>
2015-04-09 23:32:25 +0800

    GBK= =

--- COJ/Lesson/test.cpp
+++ COJ/Lesson/test.cpp
@@ -4,6 +4,7 @@ int a, b;
 
 int fun1(int a, int b)
 {
+	return b == 0 ? a : fun1(b, a % b);
 }
 
 int fun2(int a, int b)
@@ -13,5 +14,8 @@ int fun2(int a, int b)
 
 int main(int argc, char const *argv[])
 {
+	scanf("%d,%d", &a, &b);
+	printf("最大公约数：%d\n", fun1(a, b));
+	printf("最小公倍数：%d", fun2(a, b));
 	return 0;
 }
