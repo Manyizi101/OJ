dcb26672a6cb486b808d8c74b4b7d70d6b58bdf0
Xuanwo <xuanwo.cn@gmail.com>
2015-04-09 23:32:12 +0800

    求最大公约数和最小公倍数

--- COJ/Lesson/test.cpp
+++ COJ/Lesson/test.cpp
@@ -1,1 +1,17 @@
-#init
+#include <stdio.h>
+
+int a, b;
+
+int fun1(int a, int b)
+{
+}
+
+int fun2(int a, int b)
+{
+	return a * b / fun1(a, b);
+}
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
