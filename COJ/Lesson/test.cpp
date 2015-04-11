48de8761692c382799a72ad142391fc44dc60654
Xuanwo <xuanwo.cn@gmail.com>
2015-04-12 02:41:52 +0800

    C3-1

--- COJ/Lesson/test.cpp
+++ COJ/Lesson/test.cpp
@@ -22,31 +22,18 @@
 #define ll long long int
 const double pi = acos(-1.0);
 const double eps = (1e-8);
-const int inf = 1 << 31;
+const int inf = 1<<31;
 using namespace std;
 
-
-int f(int a,int b)
-
-{
-
-if(a<b)
-
-return f((b-a),a);
-
-else if(a ==b)
-
-return 2*a;
-
-else
-
-return f(f(a/2,b),f(b,b%2));
-
-}
+int a[3];
 
 int main(int argc, char const *argv[])
 {
-	cout<<f(3,5);
+	while(~scanf("%d%d%d", &a[0],&a[1],&a[2]))
+	{
+		sort(a,a+3);
+		if(a[0]*a[0]+a[1]*a[1]==a[2]*a[2])	cout<<"True"<<endl;
+		else cout<<"False"<<endl;
+	}
 	return 0;
 }
-
