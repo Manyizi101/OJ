8dbdc07b56d85ec2507ab625b4faa58b62b7e79d
Xuanwo <xuanwo.cn@gmail.com>
2015-04-12 13:54:22 +0800

    课堂练习

--- COJ/Lesson/test.cpp
+++ COJ/Lesson/test.cpp
@@ -1,46 +1,28 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <ctime>
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <deque>
-#include <list>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <numeric>
-#include <iomanip>
-#include <bitset>
-#include <sstream>
-#include <fstream>
-#define debug puts("-----")
-#define ll long long int
-const double pi = acos(-1.0);
-const double eps = (1e-8);
-const int inf = 1<<31;
+#include<iostream>
 using namespace std;
+class Clock {
+public:
+	void setTime(int newH = 0, int newM = 0, int newS = 0);
+	void showTime();
+private:
+	int hour, minute, second;
+};
 
-const int MAXN = 100;
-ll a[MAXN],n;
-
-ll f(int n)
-{
-	if(a[n]!=-1)	return a[n];
-	if(n==0||n==1)	return a[n]=1;
-	else return a[n]=f(n-1)+f(n-2);
+void Clock::setTime(int newH, int newM, int newS) {
+	hour = newH;
+	minute = newM;
+	second = newS;
+}
+void Clock::showTime() {
+	cout << hour << ":" << minute << ":" << second;
 }
 
-int main(int argc, char const *argv[])
-{
-	memset(a,-1,sizeof(a));
-	while(~scanf("%lld",&n))
-	{
-		cout<<f(n)<<endl;
-	}
+
+
+int main() {
+	Clock myClock;
+	//myClock.setTime(8, 30, 30);
+	myClock.setTime(10);
+	myClock.showTime();
 	return 0;
 }
