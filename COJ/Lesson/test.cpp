a8525d252c634618904d258be8e30c77822b9e77
Xuanwo <xuanwo.cn@gmail.com>
2015-04-12 14:06:12 +0800

    C4-1

--- COJ/Lesson/test.cpp
+++ COJ/Lesson/test.cpp
@@ -1,28 +1,37 @@
-#include<iostream>
-using namespace std;
-class Clock {
-public:
-	void setTime(int newH = 0, int newM = 0, int newS = 0);
-	void showTime();
-private:
-	int hour, minute, second;
-};
+/* students please write your program here */
 
-void Clock::setTime(int newH, int newM, int newS) {
-	hour = newH;
-	minute = newM;
-	second = newS;
-}
-void Clock::showTime() {
-	cout << hour << ":" << minute << ":" << second;
-}
+#include <iostream>
+using namespace std;
+class Integer {
+private:
+	int _num;
+public:
+//构造函数
+	Integer(int num) {
+		_num = num;
+	}
+//计算当前Integer 和 b之间的最大公约数
+	int gcd(Integer b)
+	{
+		int x = _num;
+		int y = b._num;
+		while (y != 0)
+		{
+			int r = y;
+			y = x % y;
+			x = r;
+		}
+		return x;
+	}
+};
 
 
 
 int main() {
-	Clock myClock;
-	//myClock.setTime(8, 30, 30);
-	myClock.setTime(10);
-	myClock.showTime();
+	int a, b;
+	cin >> a >> b;
+	Integer A(a);
+	Integer B(b);
+	cout << A.gcd(B) << endl;
 	return 0;
 }
