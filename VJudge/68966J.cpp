1c298f4cb9e6928ba354e2d97197ae23ac8d3258
Xuanwo <xuanwo.cn@gmail.com>
2015-07-27 16:17:36 +0800

    VJudge 68966 AC

--- VJudge/68966J.cpp
+++ VJudge/68966J.cpp
@@ -31,7 +31,8 @@ const int N = 1000 + 10;
 
 int n;
 int tw, ts;
-int b[N], ans[N];
+int dp[N];
+stack<int>ans;
 
 struct node
 {
@@ -40,66 +41,45 @@ struct node
 
 int cmp(node a, node b)
 {
-    if (a.w == b.w)    return a.s < b.s;
-    else return a.w > b.w;
-}
-
-int bSearch(int num, int k)
-{
-    int low = 1, high = k;
-    while (low <= high)
-    {
-        int mid = (low + high) / 2;
-        if (num >= b[mid])
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return low;
-}
-
-int LIS()
-{
-    int low = 1, high = n;
-    cout << n << endl;
-    int k = 1;
-    b[1] = mice[1].s;
-    ans[1] = mice[1].r;
-    for (int i = 2; i <= n; ++i)
-    {
-        if (mice[i].s >= b[k])
-        {
-            b[++k] = mice[i].s;
-            ans[k] = mice[i].r;
-        }
-        else
-        {
-            int pos = bSearch(mice[i].s, k);
-            b[pos] = mice[i].s;
-            ans[pos] = mice[i].r;
-        }
-    }
-    return k;
+    if (a.w == b.w)    return a.s > b.s;
+    else return a.w < b.w;
 }
 
 int main()
 {
-    memset(b, 0, sizeof(b));
-    memset(ans, 0, sizeof(ans));
     n = 1;
     while (~scanf("%d%d", &mice[n].w, &mice[n].s))
     {
         mice[n].r = n;
         n++;
     }
-    sort(mice + 1, mice + n , cmp);
-    //for (int i = 1; i <= n; i++)   printf("%d %d\n", mice[i].w, mice[i].s);
-    int k = LIS();
-    printf("%d\n", k);
-    for (int i = 1; i <= k; i++)
+    n--;
+    sort(mice + 1, mice + n + 1 , cmp);
+    for (int i = 0; i < n; i++)
     {
-        printf("%d\n", ans[i]);
-        //printf("%d %d\n", mice[ans[i]].w, mice[ans[i]].s);
+        int t = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (mice[i].w > mice[j].w && mice[i].s < mice[j].s)    t = max(t, dp[j]);
+            dp[i] = t + 1;
+        }
+    }
+    int ind = max_element(dp, dp + n) - dp;
+    ans.push(mice[ind].r);
+    int t = ind;
+    for (int i = ind - 1; i >= 0; i--)
+    {
+        if (mice[i].w < mice[t].w && mice[i].s > mice[t].s && dp[t] == dp[i] + 1)
+        {
+            ans.push(mice[i].r);
+            t = i;
+        }
+    }
+    printf("%d\n", dp[ind]);
+    while (!ans.empty())
+    {
+        printf("%d\n", ans.top());
+        ans.pop();
     }
     return 0;
 }
