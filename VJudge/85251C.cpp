fa6c6f976f749d2983e65550225a5c7ee92b521f
Xuanwo <xuanwo.cn@gmail.com>
2015-08-01 18:54:58 +0800

    updated at 2015/08/01 周六 18:54:57.97

--- VJudge/85251C.cpp
+++ VJudge/85251C.cpp
@@ -27,68 +27,32 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-const ll mod = 1000000007;
+const int mod = (1e9 + 7);
 
+LL euler_phi(int n) {
+    LL ans = n;
+    for (int i = 2; i <= (int)sqrt(n + 0.5); i++)
+        if (n % i == 0) {
+            ans = ans / i * (i - 1);
+            while (n % i == 0) n /= i;
+        }
+    if (n > 1) ans = ans / n * (n - 1);
+    return ans;
+}
 int n, k;
-const int NP = 1000005;
-
-int ispri[NP] = {}, prime[NP], pcnt = 0;
-
-void getprime()
-{
-    ispri[0] = ispri[1] = 1;
-    for (long long i = 2; i < NP; i++)
-        if (ispri[i] == 0)
-        {
-            prime[++pcnt] = i;
-            for (long long j = i * i; j < NP; j += i)
-                ispri[j] = 1;
-        }
-}
-
-ll cal(int n)
-{
-    int num = n;
-    ll ans = 0;
-    for (int i = 1; prime[i]*prime[i] <= n; i++)
-        if (n % prime[i] == 0)
-        {
-            n /= prime[i];
-            while (n % prime[i] == 0) n /= prime[i];
-            ans++;
-            if (n == 1) break;
-        }
-    if (n > 1) ans++;
-    return ans;
-}
-
-ll qPow(ll a, ll b, ll c)
-{
-    ll ans = 1;
-    a = a % c;
-    while (b > 0) {
-        if (b % 2 == 1)
-            ans = (ans * a) % c;
-        b = b / 2;
-        a = (a * a) % c;
-    }
-    return ans;
-}
-
 int main()
 {
-    getprime();
-    while (~scanf("%d%d", &n, &k))
-    {
-        if (k == 2 || n == 1)    cout << 1 << endl;
-        else if (k > 2 && n != 1)   cout << 0 << endl;
-        else
-        {
-            ll ans = 0;
-            ll x = cal(n);
-            ans = qPow(2, x, mod);
+    while (scanf("%d%d", &n, &k) != EOF) {
+        if (n == 1 || k == 2) puts("1");
+        else if (k > 2) puts("0");
+        else {
+            LL ans = 0;
+            for (int g = 1; g <= sqrt(n * 1.0); g++)
+                if (n % g == 0) {
+                    if (g != n / g) ans = ans + euler_phi(n / g) * euler_phi(g) * 2;
+                    else ans = ans + euler_phi(n / g) * euler_phi(g);
+                }
             printf("%I64d\n", ans % mod);
         }
     }
-    return 0;
 }
