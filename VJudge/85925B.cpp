d157f923702a5a62c4a4fc291917432795982ae5
Xuanwo <xuanwo.cn@gmail.com>
2015-08-03 11:02:27 +0800

    updated at 2015/08/03 周一 11:02:27.84

---
+++ VJudge/85925B.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+using namespace std;
+
+const int maxn = 16;
+
+int t, n;
+int a[maxn];
+
+int main()
+{
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+
+    }
+    return 0;
+}
