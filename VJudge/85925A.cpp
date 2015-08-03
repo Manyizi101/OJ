8ce45920b2ebb9b2af973c7e9c98c5d400ec2680
Xuanwo <xuanwo.cn@gmail.com>
2015-08-03 10:57:09 +0800

    updated at 2015/08/03 周一 10:57:09.22

--- VJudge/85925A.cpp
+++ VJudge/85925A.cpp
@@ -27,12 +27,46 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-vector<int> a = {1, 2, 3, 4, 5};
+vector<vector<string> > a;
+int Max[10000] = {0};
+
+void read()
+{
+    a.clear();
+    string tmp;
+    int num = 0;
+    while (getline(cin, tmp))
+    {
+        num = 0;
+        stringstream ss(tmp);
+        vector<string> x;
+        string tt;
+        while (ss >> tt)
+        {
+            Max[num] = max((int)tt.size(), Max[num]);
+            num++;
+            x.push_back(tt);
+        }
+        a.push_back(x);
+    }
+}
 
 int main()
 {
-    cout << a.size() << endl;
-    a.pop_back();
-    cout << a.size() << endl;
+    //freopen("85925A.out", "w", stdout);
+    read();
+    for (auto i = a.begin(); i != a.end(); ++i)
+    {
+        for (auto j = (*i).begin(); j != (*i).end(); ++j)
+        {
+            cout << (*j);
+            if (j != (*i).end() - 1)
+                for (int x = 0; x < (Max[j - (*i).begin()] - (int)(*j).size()) + 1; ++x)
+                {
+                    cout << " ";
+                }
+        }
+        cout << endl;
+    }
     return 0;
 }
