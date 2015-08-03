5803f6f293fa4710c1aff4d7bf91eb701d6179aa
Xuanwo <xuanwo.cn@gmail.com>
2015-08-03 11:13:05 +0800

    updated at 2015/08/03 周一 11:13:05.90

--- VJudge/85925B.cpp
+++ VJudge/85925B.cpp
@@ -30,15 +30,48 @@ using namespace std;
 const int maxn = 16;
 
 int t, n;
-int a[maxn];
+vector<int> a;
+
+int judgel(vector<int> b)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        if (b[i] != a[i]) return 1;
+    }
+    return 0;
+}
+
+int judgez(vector<int> b)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        if (b[i] != 0) return 1;
+    }
+    return 0;
+}
 
 int main()
 {
     scanf("%d", &t);
     while (t--)
     {
+        int tmp;
         scanf("%d", &n);
-
+        for (int i = 0; i < n; ++i)
+        {
+            scanf("%d", &tmp);
+            a.push_back(tmp);
+        }
+        vector<int> b(a);
+        while (judgel(b))
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                a[i % n] = abs(a[(i + 1) % n] - a[i % n]);
+            }
+        }
+        if (judgez(b) == 1) cout << "ZERO" << endl;
+        else cout << "LOOP" << endl;
     }
     return 0;
 }
