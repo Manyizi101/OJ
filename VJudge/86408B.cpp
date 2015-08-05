0047b8cb62d0c9b24ab955e7a5e7e204515616a2
Xuanwo <xuanwo.cn@gmail.com>
2015-08-05 21:06:34 +0800

    updated at 2015/08/05 周三 21:06:34.33

--- VJudge/86408B.cpp
+++ VJudge/86408B.cpp
@@ -27,44 +27,30 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-
-
-const int NP = 10000005;
-int a[NP];
-int ispri[NP] = {}, prime[NP], pcnt = 0;
-void getprime()
+ll cal(ll n, ll k)
 {
-    ispri[0] = ispri[1] = 1;
-    for (long long i = 2; i < NP; i++)
-        if (ispri[i] == 0)
-        {
-            prime[++pcnt] = i;
-            for (long long j = i * i; j < NP; j += i)
-                ispri[j] = 1;
-        }
-}
-
-ll cal(ll n)
-{
-    if (n < NP && ispri[n])
+    if (k == 1)
     {
-        a[0] = n;
-        return 1;
+        return n;
     }
-    int i = 0; // 循环标志
-    int len = 0;
+    ll i = 0; // 循环标志
+    int len = 1;
     // n>=2才执行下面的循环
-    for (i = 2; i <= n; i++) {
-        while (n != i) {
-            if (n % i == 0) {
-                a[len++] = i;
+    for (i = 2; i <= n; i++)
+    {
+        while (n != i)
+        {
+            if (n % i == 0)
+            {
+                len++;
                 n = n / i;
-            } else
+                if (k == len)  return n;
+            }
+            else
                 break;
         }
     }
-    a[len++] = n;
-    return len;
+    return -1;
 }
 
 ll gcd(ll a, ll b)
@@ -77,26 +63,16 @@ ll x, y, k;
 
 int main()
 {
-    getprime();
     scanf("%d", &t);
     while (t--)
     {
         scanf("%I64d%I64d%I64d", &x, &y, &k);
         ll num = gcd(x, y);
-        int len = cal(num);
+        ll ans = cal(num, k);
         // cout << len << endl;
         // for (int i = 0; i < len; i++)  cout << a[i] << " ";
         // cout << endl;
-        if (k > len)   printf("-1\n");
-        else
-        {
-            ll ans = 1;
-            for (int i = len - 1; i >= k - 1; i--)
-            {
-                ans *= a[i];
-            }
-            printf("%I64d\n", ans);
-        }
+        printf("%I64d\n", ans);
     }
     return 0;
 }
