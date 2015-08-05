c130594a83f5c4f4ebad1ba12be45667150e3026
Xuanwo <xuanwo.cn@gmail.com>
2015-08-05 17:56:24 +0800

    updated at 2015/08/05 周三 17:56:24.08

---
+++ VJudge/68968C.cpp
@@ -0,0 +1,154 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+using namespace std;
+
+const int maxn = 100 + 10;
+
+inline double sqr(const double &x)
+{
+    return x * x;
+}
+inline int sgn(const double &x)
+{
+    return x < -eps ? -1 : x > eps;
+}
+struct point
+{
+    double x, y;
+    point(const double &x = 0, const double &y = 0): x(x), y(y) {}
+    friend point operator + (const point &a, const point &b)
+    {
+        return point(a.x + b.x, a.y + b.y);
+    }
+    friend point operator - (const point &a, const point &b)
+    {
+        return point(a.x - b.x, a.y - b.y);
+    }
+    friend point operator * (const point &a, const double &b)
+    {
+        return point(a.x * b, a.y * b);
+    }
+    friend point operator * (const double &a, const point &b)
+    {
+        return point(a * b.x, a * b.y);
+    }
+    friend point operator / (const point &a, const double &b)
+    {
+        return point(a.x / b, a.y / b);
+    }
+    friend bool operator == (const point &a, const point &b)
+    {
+        return !sgn(a.x - b.x) && !sgn(a.y - b.y);
+    }
+    friend bool operator < (const point &a, const point &b)
+    {
+        return sgn(a.x - b.x) < 0 || (sgn(a.x - b.x) == 0 && sgn(a.y - b.y) < 0);
+    }
+    double norm()
+    {
+        return sqrt(sqr(x) + sqr(y));
+    }
+    friend double det(const point &a, const point &b)
+    {
+        return a.x * b.y - a.y * b.x;
+    }
+    friend double dot(const point &a, const point &b)
+    {
+        return a.x * b.x + a.y * b.y;
+    }
+    friend double dist(const point &a, const point &b)
+    {
+        return (a - b).norm();
+    }
+    double arg()
+    {
+        return atan2(y, x);
+//        double res = atan2(y, x);   //(-pi, pi]
+//        if(res < -pi / 2 + eps) res += 2 * pi; //eps修正精度
+//        return res;
+    }
+    //逆时针旋转angle弧度
+    point rotate(const double &angle)
+    {
+        return rotate(cos(angle), sin(angle));
+    }
+    point rotate(const point &p, const double &angle)
+    {
+        return (*this - p).rotate(angle) + p;
+    }
+    point rotate(const double &cosa, const double &sina)
+    {
+        return point(x * cosa - y * sina, x * sina + y * cosa);
+    }
+    int in()
+    {
+        return scanf("%lf %lf", &x, &y);
+    }
+    void out()
+    {
+        printf("%.2f %.2f\n", x, y);
+    }
+};
+
+int t, n;
+point p[maxn];
+
+int main()
+{
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        for (int i = 0; i < 2 * n; i += 2)
+        {
+            p[i].in();
+            p[i + 1].in();
+        }
+        bool flag = false;
+        for (int i = 0; i < 2 * n && !flag; ++i)
+        {
+            for (int j = 0; j < 2 * n; ++j)
+            {
+                point p1 = p[i], p2 = p[j];
+                if (p1 == p2)  continue;
+                int k;
+                for (k = 0; k < 2 * n; k += 2)
+                {
+                    if (det(p[k] - p1, p[k] - p2)*det(p[k + 1] - p1, p[k + 1] - p2) > eps)   break;
+                }
+                if (k >= 2 * n)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        printf(flag ? "Yes!\n" : "No!\n");
+    }
+    return 0;
+}
