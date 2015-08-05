61b44d89705c4e6e20436a41fb97b9ee25a23807
Xuanwo <xuanwo.cn@gmail.com>
2015-08-06 01:58:29 +0800

    updated at 2015/08/06 周四  1:58:29.50

--- CodeForces/567C.cpp
+++ CodeForces/567C.cpp
@@ -38,6 +38,8 @@ int main()
 {
     while (~scanf("%d%d", &n, &k))
     {
+        memset(a, 0, sizeof(a));
+        memset(b, 0, sizeof(b));
         ans = 0;
         na = nb = 0;
         ll tmp;
@@ -48,30 +50,30 @@ int main()
             {
                 a[++na] = tmp;
             }
-            else if (a[i] < 0)
+            else if (tmp < 0)
             {
                 b[++nb] = tmp;
             }
         }
-        for (int i = 2; i <= na; ++i)
+        for (int i = 2; i <= na - 1; ++i)
         {
             int ansa = 0, ansb = 0;
             if (a[i] % k != 0)   continue;
             ll tj = a[i] / k, tx = a[i] * k;
             for (int j = 1; j < i; ++j)
                 if (a[j] == tj) ansa++;
-            for (int x = i + 1; x <= n; ++x)
+            for (int x = i + 1; x <= na; ++x)
                 if (a[x] == tx) ansb++;
             ans += ansa * ansb;
         }
-        for (int i = 2; i <= nb; ++i)
+        for (int i = 2; i <= nb - 1; ++i)
         {
             int ansa = 0, ansb = 0;
             if (b[i] % k != 0)   continue;
             ll tj = b[i] / k, tx = b[i] * k;
             for (int j = 1; j < i; ++j)
                 if (b[j] == tj) ansa++;
-            for (int x = i + 1; x <= n; ++x)
+            for (int x = i + 1; x <= nb; ++x)
                 if (b[x] == tx) ansb++;
             ans += ansa * ansb;
         }
