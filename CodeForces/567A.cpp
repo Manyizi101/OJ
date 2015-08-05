91f0151daa53dbd1b85eeed8a4f02a67cb3c169c
Xuanwo <xuanwo.cn@gmail.com>
2015-08-06 00:37:30 +0800

    updated at 2015/08/06 周四  0:37:30.49

--- CodeForces/567A.cpp
+++ CodeForces/567A.cpp
@@ -27,7 +27,25 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+const int maxn = 1e5 + 5;
+
+int n;
+ll m[maxn];
+
 int main()
 {
+    while (~scanf("%d", &n))
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%I64d", &m[i]);
+        }
+        printf("%I64d %I64d\n", m[1] - m[0], m[n - 1] - m[0]);
+        for (int i = 1; i < n - 1; ++i)
+        {
+            printf("%I64d %I64d\n", min(m[i + 1] - m[i], m[i] - m[i - 1]), max(m[n - 1] - m[i], m[i] - m[0]));
+        }
+        printf("%I64d %I64d\n", m[n - 1] - m[n - 2], m[n - 1] - m[0]);
+    }
     return 0;
 }
--- CodeForces/567B.cpp
+++ CodeForces/567B.cpp
@@ -27,7 +27,43 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+int n;
+vector<int> v;
+char x;
+int tmp;
+int ans;
+
 int main()
 {
+    while (~scanf("%d", &n))
+    {
+        ans = 0;
+        v.clear();
+        for (int i = 0; i < n; i++)
+        {
+            getchar();
+            scanf("%c%d", &x, &tmp);
+            if (x == '+')
+            {
+                v.push_back(tmp);
+                ans = max(ans, (int)v.size());
+            }
+            else if (x == '-')
+            {
+                int flag = 0;
+                for (auto it = v.begin(); it != v.end(); ++it)
+                {
+                    if (*it == tmp)
+                    {
+                        flag = 1;
+                        v.erase(it);
+                        break;
+                    }
+                }
+                if (flag == 0) ans++;
+            }
+        }
+        printf("%d\n", ans);
+    }
     return 0;
 }
