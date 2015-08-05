63e31a0da5c650917197710a57fa323be5db96d5
Xuanwo <xuanwo.cn@gmail.com>
2015-08-06 01:40:10 +0800

    updated at 2015/08/06 周四  1:40:10.71

--- CodeForces/567C.cpp
+++ CodeForces/567C.cpp
@@ -27,7 +27,55 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+const int maxn = 2 * 1e5 + 10;
+
+int n, k;
+ll a[maxn], b[maxn];
+int na, nb;
+ll ans;
+
 int main()
 {
+    while (~scanf("%d%d", &n, &k))
+    {
+        ans = 0;
+        na = nb = 0;
+        ll tmp;
+        for (int i = 1; i <= n; ++i)
+        {
+            scanf("%I64d", &tmp);
+            if (tmp > 0)
+            {
+                a[++na] = tmp;
+            }
+            else if (a[i] < 0)
+            {
+                b[++nb] = tmp;
+            }
+        }
+        for (int i = 2; i <= na; ++i)
+        {
+            int ansa = 0, ansb = 0;
+            if (a[i] % k != 0)   continue;
+            ll tj = a[i] / k, tx = a[i] * k;
+            for (int j = 1; j < i; ++j)
+                if (a[j] == tj) ansa++;
+            for (int x = i + 1; x <= n; ++x)
+                if (a[x] == tx) ansb++;
+            ans += ansa * ansb;
+        }
+        for (int i = 2; i <= nb; ++i)
+        {
+            int ansa = 0, ansb = 0;
+            if (b[i] % k != 0)   continue;
+            ll tj = b[i] / k, tx = b[i] * k;
+            for (int j = 1; j < i; ++j)
+                if (b[j] == tj) ansa++;
+            for (int x = i + 1; x <= n; ++x)
+                if (b[x] == tx) ansb++;
+            ans += ansa * ansb;
+        }
+        printf("%I64d\n", ans);
+    }
     return 0;
 }
