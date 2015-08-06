10dec136345c8071632798d627dcf4f9d87a9b4f
Xuanwo <xuanwo.cn@gmail.com>
2015-08-06 21:07:28 +0800

    updated at 2015/08/06 周四 21:07:28.40

--- VJudge/test.cpp
+++ VJudge/test.cpp
@@ -1,183 +1,130 @@
-#include<cstdio>
-#include<cstring>
-#include<cmath>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<map>
-#include<set>
+/*
+最大流模板
+sap
+效率更高
+起点和终点没有要求
+传送的参数要大于总节点数量
+*/
+#include<stdio.h>
+#include<string.h>
 #include<algorithm>
 #include<iostream>
-#include<string>
-typedef long long ll;
 using namespace std;
-int a[110000];
-int vis[110000];
-int main()
-{
-    // freopen("test.in", "r", stdin);
-    // freopen("test.out", "w", stdout);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        ll sum = 0;
-        int n;
-        cin >> n;
-        for (int i = 0; i < n; i++)
-        {
-            scanf("%d", &a[i]);
-            sum += a[i];
-        }
-        if (sum % n)
-        {
-            printf("NO\n");
-            continue;
-        }
-        int flag = 1;
-        ll ed = sum / n;
-        for (int i = 0; i < n; i++)
-        {
-            if (abs(a[i] - ed) > 2)
-                flag  = 0 ;
-        }
-        if (!flag)
-        {
-            printf("NO\n");
-            continue;
-        }
-        memset(vis, 0, sizeof(vis));
-        for (int i = 0; i < n; i++)
-        {
-            if (a[i] == ed)
-                continue;
-            if (a[i] < ed)
-            {
-                if (a[i] + 1 == ed)
-                {
-                    if (a[(i - 1 + n) % n] > a[(i + 1 + n) % n])
-                    {
-                        if (!vis[(i - 1 + n) % n])
-                        {
-                            vis[(i - 1 + n) % n] = 1;
-                            a[i] = ed;
-                            a[(i - 1 + n) % n]--;
-                        }
-                        else if (! vis[i])
-                        {
-                            vis[i] = -1;
-                            a[i] = ed;
-                            a[(i + 1 + n) % n]--;
-                        }
-                    }
-                    else
-                    {
-                        if (!vis[i])
-                        {
-                            vis[i] = -1;
-                            a[i] = ed;
-                            a[(i + 1 + n) % n]--;
-                        }
-                        else if (!vis[(i - 1 + n) % n])
-                        {
-                            vis[(i - 1 + n) % n] = 1;
-                            a[i] = ed;
-                            a[(i - 1 + n) % n]--;
-                        }
-                    }
-                }
-                else if (a[i] + 2 == ed)
-                {
-                    if (!vis[(i - 1 + n) % n] && !vis[i])
-                    {
-                        a[i] = ed;
-                        a[(i + 1 + n) % n]--;
-                        a[(i - 1 + n) % n]--;
-                        vis[(i - 1 + n) % n] = 1;
-                        vis[i] = -1;
-                    }
-                }
-            }
-            else
-            {
-                if (a[i] - 1 == ed)
-                {
-                    if (a[(i - 1 + n) % n] >= a[(i + 1 + n) % n])
-                    {
-                        if (!vis[i])
-                        {
-                            a[(i + 1 + n) % n]++;
-                            vis[i] = 1;
-                            a[i] = ed;
-                        }
-                        else if (!vis[(i - 1 + n) % n])
-                        {
-                            a[(i - 1 + n) % n]++;
-                            vis[(i - 1 + n) % n] = -1;
-                            a[i] = ed;
-                        }
-                    }
-                    else
-                    {
-                        if (!vis[(i - 1 + n) % n])
-                        {
-                            a[(i - 1 + n) % n]++;
-                            vis[(i - 1 + n) % n] = -1;
-                            a[i] = ed;
-                        }
-                        else if (!vis[i])
-                        {
-                            a[(i + 1 + n) % n]++;
-                            vis[i] = 1;
-                            a[i] = ed;
-                        }
-                    }
-                }
-                else  if (a[i] - 2 == ed)
-                {
-                    if (!vis[i] && !vis[(i - 1 + n) % n])
-                    {
-                        a[(i - 1 + n) % n]++;
-                        a[(i + 1 + n) % n]++;
-                        vis[i] = 1;
-                        vis[(i - 1 + n) % n] = -1;
-                        a[i] = ed;
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < n; i++)
-        {
-            //  cout<<a[i]<<" ";
-            if (a[i] != ed)
-                flag = 0;
-        }
-        //  cout<<endl;
-        if (!flag)
-        {
-            cout << "NO\n";
-            continue;
-        }
-        cout << "YES\n";
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-            if (vis[i]) ans++;
-        cout << ans << endl;
-        int xx, yy;
-        for (int i = 0; i < n; i++)
-        {
-//            if(vis[i])
-//                cout<<i+1<<" "<<vis[i]<<endl;
-            xx = i + 1;
-            yy = i + 2;
-            if (yy == n + 1)
-                yy = 1;
-            if (vis[i] == 1)
 
-                printf("%d %d\n", xx, yy);
-            else if (vis[i] == -1)
-                printf("%d %d\n", yy, xx);
+const int MAXN = 100010; //点数的最大值
+const int MAXM = 400010; //边数的最大值
+const int INF = 0x3f3f3f3f;
+struct Node
+{
+    int from, to, next;
+    int cap;
+} edge[MAXM];
+int tol;
+int head[MAXN];
+int dep[MAXN];
+int gap[MAXN];//gap[x]=y :说明残留网络中dep[i]==x的个数为y
+void init()
+{
+    tol = 0;
+    memset(head, -1, sizeof(head));
+}
+void addedge(int u, int v, int w)
+{
+    edge[tol].from = u;
+    edge[tol].to = v;
+    edge[tol].cap = w;
+    edge[tol].next = head[u];
+    head[u] = tol++;
+    edge[tol].from = v;
+    edge[tol].to = u;
+    edge[tol].cap = 0;
+    edge[tol].next = head[v];
+    head[v] = tol++;
+}
+void BFS(int start, int end)
+{
+    memset(dep, -1, sizeof(dep));
+    memset(gap, 0, sizeof(gap));
+    gap[0] = 1;
+    int que[MAXN];
+    int front, rear;
+    front = rear = 0;
+    dep[end] = 0;
+    que[rear++] = end;
+    while (front != rear)
+    {
+        int u = que[front++];
+        if (front == MAXN)front = 0;
+        for (int i = head[u]; i != -1; i = edge[i].next)
+        {
+            int v = edge[i].to;
+            if (dep[v] != -1)continue;
+            que[rear++] = v;
+            if (rear == MAXN)rear = 0;
+            dep[v] = dep[u] + 1;
+            ++gap[dep[v]];
         }
     }
-    return 0;
+}
+int sap(int start, int end, int n)
+{
+    int res = 0;
+    BFS(start, end);
+    int cur[MAXN];
+    int S[MAXN];
+    int top = 0;
+    memcpy(cur, head, sizeof(head));
+    int u = start;
+    int i;
+    while (dep[start] < n)
+    {
+        if (u == end)
+        {
+            int temp = INF;
+            int inser;
+            for (i = 0; i < top; i++)
+                if (temp > edge[S[i]].cap)
+                {
+                    temp = edge[S[i]].cap;
+                    inser = i;
+                }
+            for (i = 0; i < top; i++)
+            {
+                edge[S[i]].cap -= temp;
+                edge[S[i] ^ 1].cap += temp;
+            }
+            res += temp;
+            top = inser;
+            u = edge[S[top]].from;
+        }
+        if (u != end && gap[dep[u] - 1] == 0) //出现断层，无增广路
+            break;
+        for (i = cur[u]; i != -1; i = edge[i].next)
+            if (edge[i].cap != 0 && dep[u] == dep[edge[i].to] + 1)
+                break;
+        if (i != -1)
+        {
+            cur[u] = i;
+            S[top++] = i;
+            u = edge[i].to;
+        }
+        else
+        {
+            int min = n;
+            for (i = head[u]; i != -1; i = edge[i].next)
+            {
+                if (edge[i].cap == 0)continue;
+                if (min > dep[edge[i].to])
+                {
+                    min = dep[edge[i].to];
+                    cur[u] = i;
+                }
+            }
+            --gap[dep[u]];
+            dep[u] = min + 1;
+            ++gap[dep[u]];
+            if (u != start)u = edge[S[--top]].from;
+        }
+    }
+    return res;
 }
