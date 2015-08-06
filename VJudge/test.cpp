fe64198f86befe2fb1d7eefe30018fd44fa9ac05
Xuanwo <xuanwo.cn@gmail.com>
2015-08-06 21:57:15 +0800

    updated at 2015/08/06 周四 21:57:15.33

--- VJudge/test.cpp
+++ VJudge/test.cpp
@@ -1,130 +1,36 @@
-/*
-最大流模板
-sap
-效率更高
-起点和终点没有要求
-传送的参数要大于总节点数量
-*/
-#include<stdio.h>
-#include<string.h>
-#include<algorithm>
-#include<iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-const int MAXN = 100010; //点数的最大值
-const int MAXM = 400010; //边数的最大值
-const int INF = 0x3f3f3f3f;
-struct Node
+char a[100];
+
+int main()
 {
-    int from, to, next;
-    int cap;
-} edge[MAXM];
-int tol;
-int head[MAXN];
-int dep[MAXN];
-int gap[MAXN];//gap[x]=y :说明残留网络中dep[i]==x的个数为y
-void init()
-{
-    tol = 0;
-    memset(head, -1, sizeof(head));
-}
-void addedge(int u, int v, int w)
-{
-    edge[tol].from = u;
-    edge[tol].to = v;
-    edge[tol].cap = w;
-    edge[tol].next = head[u];
-    head[u] = tol++;
-    edge[tol].from = v;
-    edge[tol].to = u;
-    edge[tol].cap = 0;
-    edge[tol].next = head[v];
-    head[v] = tol++;
-}
-void BFS(int start, int end)
-{
-    memset(dep, -1, sizeof(dep));
-    memset(gap, 0, sizeof(gap));
-    gap[0] = 1;
-    int que[MAXN];
-    int front, rear;
-    front = rear = 0;
-    dep[end] = 0;
-    que[rear++] = end;
-    while (front != rear)
-    {
-        int u = que[front++];
-        if (front == MAXN)front = 0;
-        for (int i = head[u]; i != -1; i = edge[i].next)
-        {
-            int v = edge[i].to;
-            if (dep[v] != -1)continue;
-            que[rear++] = v;
-            if (rear == MAXN)rear = 0;
-            dep[v] = dep[u] + 1;
-            ++gap[dep[v]];
-        }
-    }
-}
-int sap(int start, int end, int n)
-{
-    int res = 0;
-    BFS(start, end);
-    int cur[MAXN];
-    int S[MAXN];
-    int top = 0;
-    memcpy(cur, head, sizeof(head));
-    int u = start;
-    int i;
-    while (dep[start] < n)
-    {
-        if (u == end)
-        {
-            int temp = INF;
-            int inser;
-            for (i = 0; i < top; i++)
-                if (temp > edge[S[i]].cap)
-                {
-                    temp = edge[S[i]].cap;
-                    inser = i;
-                }
-            for (i = 0; i < top; i++)
-            {
-                edge[S[i]].cap -= temp;
-                edge[S[i] ^ 1].cap += temp;
-            }
-            res += temp;
-            top = inser;
-            u = edge[S[top]].from;
-        }
-        if (u != end && gap[dep[u] - 1] == 0) //出现断层，无增广路
-            break;
-        for (i = cur[u]; i != -1; i = edge[i].next)
-            if (edge[i].cap != 0 && dep[u] == dep[edge[i].to] + 1)
-                break;
-        if (i != -1)
-        {
-            cur[u] = i;
-            S[top++] = i;
-            u = edge[i].to;
-        }
-        else
-        {
-            int min = n;
-            for (i = head[u]; i != -1; i = edge[i].next)
-            {
-                if (edge[i].cap == 0)continue;
-                if (min > dep[edge[i].to])
-                {
-                    min = dep[edge[i].to];
-                    cur[u] = i;
-                }
-            }
-            --gap[dep[u]];
-            dep[u] = min + 1;
-            ++gap[dep[u]];
-            if (u != start)u = edge[S[--top]].from;
-        }
-    }
-    return res;
+    gets(a);
+    return 0;
 }
