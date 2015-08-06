be1795d6d05906282ce9a82197ca5cc97a56dfaf
Xuanwo <xuanwo.cn@gmail.com>
2015-08-06 20:51:22 +0800

    updated at 2015/08/06 周四 20:51:22.27

--- HOJ/Contests/594-1001.cpp
+++ HOJ/Contests/594-1001.cpp
@@ -53,71 +53,86 @@ int main()
             scanf("%I64d", &a[i]);
             sum += a[i];
         }
+        if (n == 2)
+        {
+            if (a[2] == a[1])
+            {
+                printf("YES\n");
+            }
+            else if (a[2] == a[1] + 2)
+            {
+                printf("YES\n2 1\n");
+            }
+            else if (a[1] == a[2] + 2)
+            {
+                printf("YES\n1 2\n");
+            }
+            else
+            {
+                printf("NO\n");
+            }
+            continue;
+        }
         if (sum % n != 0)    flag = 0;
         else
         {
             ll divide = sum / n;
-            for (int i = 1; i <= n; ++i)        b[i] = a[i] - divide;
-            if (abs(b[1]) > 2)
+            for (int i = 1; i <= n; ++i)
             {
-                flag = 0;
-                continue;
-            }
-            if (b[1] > 0)
-            {
-                if (b[2] < b[n])
-                {
-                    b[2]++;
-                    b[1]--;
-                    ans[anss].st = 1;
-                    ans[anss++].ed = 2;
-                }
-                else
-                {
-                    b[n]++;
-                    b[1]--;
-                    ans[anss].st = 1;
-                    ans[anss++].ed = n;
-                }
-            }
-            else
-            {
-                if (b[2] > b[n])
-                {
-                    b[2]--;
-                    b[1]++;
-                    ans[anss].st = 2;
-                    ans[anss++].ed = 1;
-                }
-                else
-                {
-                    b[n]--;
-                    b[1]++;
-                    ans[anss].st = n;
-                    ans[anss++].ed = 1;
-                }
-            }
-            for (int i = 2; i < n ; ++i)
-            {
+                b[i] = a[i] - divide;
                 if (abs(b[i]) > 2)
                 {
                     flag = 0;
-                    break;
+                    goto fuck;
                 }
-                if (b[i] > 0)
+            }
+            if (b[1] == 1)
+            {
+                b[2]--;
+                b[1]++;
+                ans[anss].st = 2;
+                ans[anss++].ed = 1;
+            }
+            else if  (b[1] < -1)
+            {
+                b[2]++;
+                b[1]--;
+                ans[anss].st = 1;
+                ans[anss++].ed = 2;
+            }
+            for (int i = 2; i < n ; ++i)
+            {
+                if (b[i] == 1)
                 {
-                    b[i + 1]++, b[i]--;
+                    b[i]--;
+                    b[i + 1]++;
                     ans[anss].st = i;
-                    ans[anss++].ed = i + 1;
+                    ans[anss++].ed = i + 1 ;
                 }
-                else if (b[i] < 0)
+                else if  (b[i] == -1)
                 {
-                    b[i + 1]--, b[i]++;
+                    b[i]++;
+                    b[i + 1]--;
                     ans[anss].st = i + 1;
                     ans[anss++].ed = i;
                 }
             }
+            if (b[n] == 1)
+            {
+                b[n]--;
+                b[1]++;
+                ans[anss].st = n;
+                ans[anss++].ed = 1 ;
+            }
+            else if  (b[n] == -1)
+            {
+                b[n]++;
+                b[1]--;
+                ans[anss].st = 1;
+                ans[anss++].ed = n;
+            }
         }
+fuck:
         if (flag == 0)     printf("NO\n");
         else
         {
--- VJudge/test.cpp
+++ VJudge/test.cpp
@@ -1,56 +1,183 @@
-//定义每个Byte中有8个Bit位
-#include ＜memory.h＞
-#define BYTESIZE 8
-void SetBit(char *p, int posi)
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<iostream>
+#include<string>
+typedef long long ll;
+using namespace std;
+int a[110000];
+int vis[110000];
+int main()
 {
-    for (int i = 0; i < (posi / BYTESIZE); i++)
+    // freopen("test.in", "r", stdin);
+    // freopen("test.out", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--)
     {
-        p++;
-    }
-
-    *p = *p | (0x01 << (posi % BYTESIZE)); //将该Bit位赋值1
-    return;
-}
-
-void BitMapSortDemo()
-{
-    //为了简单起见，我们不考虑负数
-    int num[] = {3, 5, 2, 10, 6, 12, 8, 14, 9};
-
-    //BufferLen这个值是根据待排序的数据中最大值确定的
-    //待排序中的最大值是14，因此只需要2个Bytes(16个Bit)
-    //就可以了。
-    const int BufferLen = 2;
-    char *pBuffer = new char[BufferLen];
-
-    //要将所有的Bit位置为0，否则结果不可预知。
-    memset(pBuffer, 0, BufferLen);
-    for (int i = 0; i < 9; i++)
-    {
-        //首先将相应Bit位上置为1
-        SetBit(pBuffer, num[i]);
-    }
-
-    //输出排序结果
-    for (int i = 0; i < BufferLen; i++) //每次处理一个字节(Byte)
-    {
-        for (int j = 0; j < BYTESIZE; j++) //处理该字节中的每个Bit位
+        ll sum = 0;
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
         {
-            //判断该位上是否是1，进行输出，这里的判断比较笨。
-            //首先得到该第j位的掩码（0x01＜＜j），将内存区中的
-            //位和此掩码作与操作。最后判断掩码是否和处理后的
-            //结果相同
-            if ((*pBuffer & (0x01 << j)) == (0x01 << j))
+            scanf("%d", &a[i]);
+            sum += a[i];
+        }
+        if (sum % n)
+        {
+            printf("NO\n");
+            continue;
+        }
+        int flag = 1;
+        ll ed = sum / n;
+        for (int i = 0; i < n; i++)
+        {
+            if (abs(a[i] - ed) > 2)
+                flag  = 0 ;
+        }
+        if (!flag)
+        {
+            printf("NO\n");
+            continue;
+        }
+        memset(vis, 0, sizeof(vis));
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == ed)
+                continue;
+            if (a[i] < ed)
             {
-                printf("%d ", i * BYTESIZE + j);
+                if (a[i] + 1 == ed)
+                {
+                    if (a[(i - 1 + n) % n] > a[(i + 1 + n) % n])
+                    {
+                        if (!vis[(i - 1 + n) % n])
+                        {
+                            vis[(i - 1 + n) % n] = 1;
+                            a[i] = ed;
+                            a[(i - 1 + n) % n]--;
+                        }
+                        else if (! vis[i])
+                        {
+                            vis[i] = -1;
+                            a[i] = ed;
+                            a[(i + 1 + n) % n]--;
+                        }
+                    }
+                    else
+                    {
+                        if (!vis[i])
+                        {
+                            vis[i] = -1;
+                            a[i] = ed;
+                            a[(i + 1 + n) % n]--;
+                        }
+                        else if (!vis[(i - 1 + n) % n])
+                        {
+                            vis[(i - 1 + n) % n] = 1;
+                            a[i] = ed;
+                            a[(i - 1 + n) % n]--;
+                        }
+                    }
+                }
+                else if (a[i] + 2 == ed)
+                {
+                    if (!vis[(i - 1 + n) % n] && !vis[i])
+                    {
+                        a[i] = ed;
+                        a[(i + 1 + n) % n]--;
+                        a[(i - 1 + n) % n]--;
+                        vis[(i - 1 + n) % n] = 1;
+                        vis[i] = -1;
+                    }
+                }
+            }
+            else
+            {
+                if (a[i] - 1 == ed)
+                {
+                    if (a[(i - 1 + n) % n] >= a[(i + 1 + n) % n])
+                    {
+                        if (!vis[i])
+                        {
+                            a[(i + 1 + n) % n]++;
+                            vis[i] = 1;
+                            a[i] = ed;
+                        }
+                        else if (!vis[(i - 1 + n) % n])
+                        {
+                            a[(i - 1 + n) % n]++;
+                            vis[(i - 1 + n) % n] = -1;
+                            a[i] = ed;
+                        }
+                    }
+                    else
+                    {
+                        if (!vis[(i - 1 + n) % n])
+                        {
+                            a[(i - 1 + n) % n]++;
+                            vis[(i - 1 + n) % n] = -1;
+                            a[i] = ed;
+                        }
+                        else if (!vis[i])
+                        {
+                            a[(i + 1 + n) % n]++;
+                            vis[i] = 1;
+                            a[i] = ed;
+                        }
+                    }
+                }
+                else  if (a[i] - 2 == ed)
+                {
+                    if (!vis[i] && !vis[(i - 1 + n) % n])
+                    {
+                        a[(i - 1 + n) % n]++;
+                        a[(i + 1 + n) % n]++;
+                        vis[i] = 1;
+                        vis[(i - 1 + n) % n] = -1;
+                        a[i] = ed;
+                    }
+                }
             }
         }
-        pBuffer++;
-    }
-}
+        for (int i = 0; i < n; i++)
+        {
+            //  cout<<a[i]<<" ";
+            if (a[i] != ed)
+                flag = 0;
+        }
+        //  cout<<endl;
+        if (!flag)
+        {
+            cout << "NO\n";
+            continue;
+        }
+        cout << "YES\n";
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            if (vis[i]) ans++;
+        cout << ans << endl;
+        int xx, yy;
+        for (int i = 0; i < n; i++)
+        {
+//            if(vis[i])
+//                cout<<i+1<<" "<<vis[i]<<endl;
+            xx = i + 1;
+            yy = i + 2;
+            if (yy == n + 1)
+                yy = 1;
+            if (vis[i] == 1)
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-    BitMapSortDemo();
+                printf("%d %d\n", xx, yy);
+            else if (vis[i] == -1)
+                printf("%d %d\n", yy, xx);
+        }
+    }
     return 0;
 }
