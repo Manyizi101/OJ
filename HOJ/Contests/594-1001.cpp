af59f798e1819fd2180b8da9eb31ec9a747080ca
Xuanwo <xuanwo.cn@gmail.com>
2015-08-06 15:34:55 +0800

    updated at 2015/08/06 周四 15:34:55.69

--- HOJ/Contests/594-1001.cpp
+++ HOJ/Contests/594-1001.cpp
@@ -31,32 +31,101 @@ const int maxn = 1e5 + 10;
 
 int n, t;
 ll a[maxn], sum, b[maxn];
-int flag = 0;
+int flag = 1;
+int anss;
+
+struct node
+{
+    int st, ed;
+} ans[maxn];
 
 int main()
 {
     scanf("%d", &t);
     while (t--)
     {
-        flag = 0;
+        anss = 0;
+        flag = 1;
+        sum = 0;
         scanf("%d", &n);
-        for (int i = 0; i < n; ++i)
+        for (int i = 1; i <= n; ++i)
         {
             scanf("%I64d", &a[i]);
             sum += a[i];
         }
-
-        if (sum % n == 0)    flag = 0;
+        if (sum % n != 0)    flag = 0;
         else
         {
             ll divide = sum / n;
-            for (int i = 0; i < n; ++i)
+            for (int i = 1; i <= n; ++i)        b[i] = a[i] - divide;
+            if (abs(b[1]) > 2)
             {
-                b[i] = a[i] - divide;
+                flag = 0;
+                continue;
             }
-            for(int i=0;i<n;++i)
+            if (b[1] > 0)
             {
-                if(min(b[i]-b)
+                if (b[2] < b[n])
+                {
+                    b[2]++;
+                    b[1]--;
+                    ans[anss].st = 1;
+                    ans[anss++].ed = 2;
+                }
+                else
+                {
+                    b[n]++;
+                    b[1]--;
+                    ans[anss].st = 1;
+                    ans[anss++].ed = n;
+                }
+            }
+            else
+            {
+                if (b[2] > b[n])
+                {
+                    b[2]--;
+                    b[1]++;
+                    ans[anss].st = 2;
+                    ans[anss++].ed = 1;
+                }
+                else
+                {
+                    b[n]--;
+                    b[1]++;
+                    ans[anss].st = n;
+                    ans[anss++].ed = 1;
+                }
+            }
+            for (int i = 2; i < n ; ++i)
+            {
+                if (abs(b[i]) > 2)
+                {
+                    flag = 0;
+                    break;
+                }
+                if (b[i] > 0)
+                {
+                    b[i + 1]++, b[i]--;
+                    ans[anss].st = i;
+                    ans[anss++].ed = i + 1;
+                }
+                else if (b[i] < 0)
+                {
+                    b[i + 1]--, b[i]++;
+                    ans[anss].st = i + 1;
+                    ans[anss++].ed = i;
+                }
+            }
+        }
+        if (flag == 0)     printf("NO\n");
+        else
+        {
+            printf("YES\n");
+            printf("%d\n", anss);
+            for (int i = 0; i < anss; ++i)
+            {
+                printf("%d %d\n", ans[i].st, ans[i].ed);
             }
         }
     }
