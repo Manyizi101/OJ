fce93046a9396cba5ffadef39415dd805d9a7abd
Xuanwo <xuanwo.cn@gmail.com>
2015-08-12 20:14:37 +0800

    updated at 2015/08/12 周三 20:14:37.86

---
+++ VJudge/87572A.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+using namespace std;
+
+ll v,v0;
+
+int main()
+{
+    while(~scanf("%I64d%I64d",&v,&v0))
+    {
+        printf("%I64d\n",2*v-v0);
+    }
+}
