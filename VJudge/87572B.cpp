044762f841464a5b830474122ade4a69f8556931
Xuanwo <xuanwo.cn@gmail.com>
2015-08-13 03:34:27 +0800

    updated at 2015/08/13 周四  3:34:27.71

--- VJudge/87572B.cpp
+++ VJudge/87572B.cpp
@@ -30,27 +30,54 @@ using namespace std;
 const int maxn = 50+10;
 
 int n,m;
-int a[maxn][maxn];
-int ansx,ansy;
+int b[maxn];
+int len=0;
+int ans,Max;
+int tmp;
 
+struct node
+{
+    int x,y;
+} cnt[maxn];
+
+inline int cal(node a, node b)
+{
+    return abs(a.x-b.x)+abs(a.y-b.y);
+}
 
 int main()
 {
     while(~scanf("%d%d", &n,&m))
     {
-        ansx=0,ansy=0;
+        len=0;
+        Max=inf;
         for(int i=1; i<=n; ++i)
         {
             for(int j=1; j<=m; ++j)
             {
-                scanf("%d", &a[i][j]);
-                if(a[i][j]>0)
+                scanf("%d", &tmp);
+                if(tmp>0)
                 {
-                    ansx=max(ansx,i);
-                    ansy=max(ansy,j);
+                    if(i==1&&j==1)  continue;
+                    b[len]=len;
+                    cnt[len].x=i;
+                    cnt[len].y=j;
+                    ++len;
                 }
             }
         }
-        printf("%d\n", ansx+ansy);
+        do
+        {
+            ans=0;
+            ans+=cnt[b[0]].x+cnt[b[0]].y+cnt[b[len-1]].x+cnt[b[len-1]].y;
+            for(int i=2; i<len-1; ++i)
+            {
+                ans+=cal(cnt[b[i]],cnt[b[i-1]]);
+                if(ans>Max) break;
+            }
+            Max=min(ans,Max);
+        }
+        while(next_permutation(b,b+len));
+        printf("%d\n",Max);
     }
 }
