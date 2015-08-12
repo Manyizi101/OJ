d1dc40fdfc6df55751cdabdb7ad46a9754fa5b4a
Xuanwo <xuanwo.cn@gmail.com>
2015-08-12 20:59:30 +0800

    updated at 2015/08/12 周三 20:59:30.10

--- VJudge/test.cpp
+++ VJudge/test.cpp
@@ -1,134 +1,108 @@
-#include <iostream>
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
 #include <cstring>
-const int MAXL = 10011, MAXN = 6;
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
-struct SuffixArray
-{
-    struct RadixElement
-    {
-        int id, k[2];
-    } RE[MAXL], RT[MAXL];
-    int N, A[MAXL], SA[MAXL], Rank[MAXL], Height[MAXL], C[MAXL];
-    void RadixSort()
-    {
-        int i, y;
-        for (y = 1; y >= 0; y--)
-        {
-            memset(C, 0, sizeof(C));
-            for (i = 1; i <= N; i++) C[RE[i].k[y]]++;
-            for (i = 1; i < MAXL; i++) C[i] += C[i - 1];
-            for (i = N; i >= 1; i--) RT[C[RE[i].k[y]]--] = RE[i];
-            for (i = 1; i <= N; i++) RE[i] = RT[i];
-        }
-        for (i = 1; i <= N; i++)
-        {
-            Rank[ RE[i].id ] = Rank[ RE[i - 1].id ];
-            if (RE[i].k[0] != RE[i - 1].k[0] || RE[i].k[1] != RE[i - 1].k[1])
-                Rank[ RE[i].id ]++;
-        }
-    }
-    void CalcSA()
-    {
-        int i, k;
-        RE[0].k[0] = -1;
-        for (i = 1; i <= N; i++)
-            RE[i].id = i, RE[i].k[0] = A[i], RE[i].k[1] = 0;
-        RadixSort();
-        for (k = 1; k + 1 <= N; k *= 2)
-        {
-            for (i = 1; i <= N; i++)
-                RE[i].id = i, RE[i].k[0] = Rank[i], RE[i].k[1] = i + k <= N ? Rank[i + k] : 0;
-            RadixSort();
-        }
-        for (i = 1; i <= N; i++)
-            SA[ Rank[i] ] = i;
-    }
-    void CalcHeight()
-    {
-        int i, k, h = 0;
-        for (i = 1; i <= N; i++)
-        {
-            if (Rank[i] == 1)
-                h = 0;
-            else
-            {
-                k = SA[Rank[i] - 1];
-                if (--h < 0) h = 0;
-                for (; A[i + h] == A[k + h]; h++);
-            }
-            Height[Rank[i]] = h;
-        }
-    }
-} SA;
 
-int N, Ans, Bel[MAXL];
-char S[MAXL];
+#define GetI() (SA12[t] < n0 ? SA12[t] * 3 + 1 : (SA12[t] - n0) * 3 + 2)
 
-void init()
-{
-    int i;
-    scanf("%d", &N);
-    SA.N = 0;
-    for (i = 1; i <= N; i++)
-    {
-        scanf("%s", S);
-        for (char *p = S; *p; p++)
-        {
-            SA.A[++SA.N] = *p - 'a' + 1;
-            Bel[SA.N] = i;
-        }
-        if (i < N)
-            SA.A[++SA.N] = 30 + i;
-    }
+inline bool leq(int a1, int a2, int b1, int b2) // lexicographic order
+{ return (a1 < b1 || a1 == b1 && a2 <= b2); } // for pairs
+inline bool leq(int a1, int a2, int a3, int b1, int b2, int b3)
+{ return (a1 < b1 || a1 == b1 && leq(a2, a3, b2, b3)); } // and triples
+// stably sort a[0..n-1] to b[0..n-1] with keys in 0..K from r
+static void radixPass(int* a, int* b, int* r, int n, int K)
+{   // count occurrences
+    int* c = new int[K + 1]; // counter array
+    for (int i = 0; i <= K; i++) c[i] = 0; // reset counters
+    for (int i = 0; i < n; i++) c[r[a[i]]]++; // count occurrences
+    for (int i = 0, sum = 0; i <= K; i++) // exclusive prefix sums
+    { int t = c[i]; c[i] = sum; sum += t; }
+    for (int i = 0; i < n; i++) b[c[r[a[i]]]++] = a[i]; // sort
+    delete [] c;
 }
-bool check(int A)
-{
-    int i, j, k;
-    bool ba[MAXN];
-    for (i = 1; i <= SA.N; i++)
-    {
-        if (SA.Height[i] >= A)
-        {
-            for (j = i; SA.Height[j] >= A && j <= SA.N; j++);
-            j--;
-            memset(ba, 0, sizeof(ba));
-            for (k = i - 1; k <= j; k++)
-                ba[Bel[SA.SA[k]]] = true;
-            for (k = 1; ba[k] && k <= N; k++);
-            if (k == N + 1)
-                return true;
-            i = j;
+// find the suffix array SA of T[0..n-1] in {1..K}^n
+// require T[n]=T[n+1]=T[n+2]=0, n>=2
+void suffixArray(int* T, int* SA, int n, int K) {
+    int n0 = (n + 2) / 3, n1 = (n + 1) / 3, n2 = n / 3, n02 = n0 + n2;
+    int* R = new int[n02 + 3]; R[n02] = R[n02 + 1] = R[n02 + 2] = 0;
+    int* SA12 = new int[n02 + 3]; SA12[n02] = SA12[n02 + 1] = SA12[n02 + 2] = 0;
+    int* R0 = new int[n0];
+    int* SA0 = new int[n0];
+//******* Step 0: Construct sample ********
+// generate positions of mod 1 and mod 2 suffixes
+// the "+(n0-n1)" adds a dummy mod 1 suffix if n%3 == 1
+    for (int i = 0, j = 0; i < n + (n0 - n1); i++) if (i % 3 != 0) R[j++] = i;
+//******* Step 1: Sort sample suffixes ********
+// lsb radix sort the mod 1 and mod 2 triples
+    radixPass(R , SA12, T + 2, n02, K);
+    radixPass(SA12, R , T + 1, n02, K);
+    radixPass(R , SA12, T , n02, K);
+// find lexicographic names of triples and
+// write them to correct places in R
+    int name = 0, c0 = -1, c1 = -1, c2 = -1;
+    for (int i = 0; i < n02; i++) {
+        if (T[SA12[i]] != c0 || T[SA12[i] + 1] != c1 || T[SA12[i] + 2] != c2)
+        { name++; c0 = T[SA12[i]]; c1 = T[SA12[i] + 1]; c2 = T[SA12[i] + 2]; }
+        if (SA12[i] % 3 == 1) { R[SA12[i] / 3] = name; } // write to R1
+        else { R[SA12[i] / 3 + n0] = name; } // write to R2
+    }
+// recurse if names are not yet unique
+    if (name < n02) {
+        suffixArray(R, SA12, n02, name);
+// store unique names in R using the suffix array
+        for (int i = 0; i < n02; i++) R[SA12[i]] = i + 1;
+    } else // generate the suffix array of R directly
+        for (int i = 0; i < n02; i++) SA12[R[i] - 1] = i;
+//******* Step 2: Sort nonsample suffixes ********
+// stably sort the mod 0 suffixes from SA12 by their first character
+    for (int i = 0, j = 0; i < n02; i++) if (SA12[i] < n0) R0[j++] = 3 * SA12[i];
+    radixPass(R0, SA0, T, n0, K);
+//******* Step 3: Merge ********
+// merge sorted SA0 suffixes and sorted SA12 suffixes
+    for (int p = 0, t = n0 - n1, k = 0; k < n; k++) {
+        int i = GetI(); // pos of current offset 12 suffix
+        int j = SA0[p]; // pos of current offset 0 suffix
+        if (SA12[t] < n0 ? // different compares for mod 1 and mod 2 suffixes
+                leq(T[i], R[SA12[t] + n0], T[j], R[j / 3]) :
+                leq(T[i], T[i + 1], R[SA12[t] - n0 + 1], T[j], T[j + 1], R[j / 3 + n0]))
+        {   // suffix from SA12 is smaller
+            SA[k] = i; t++;
+            if (t == n02) // done --- only SA0 suffixes left
+                for (k++; p < n0; p++, k++) SA[k] = SA0[p];
+        } else { // suffix from SA0 is smaller
+            SA[k] = j; p++;
+            if (p == n0) // done --- only SA12 suffixes left
+                for (k++; t < n02; t++, k++) SA[k] = GetI();
         }
     }
-    return false;
-}
-
-void solve()
-{
-    int a, b, m;
-    SA.CalcSA();
-    SA.CalcHeight();
-    a = 0; b = SA.N;
-    while (a + 1 < b)
-    {
-        m = (a + b) / 2;
-        if (check(m))
-            a = m;
-        else
-            b = m - 1;
-    }
-    if (check(b))
-        a = b;
-    Ans = a;
+    delete [] R; delete [] SA12; delete [] SA0; delete [] R0;
 }
 
 int main()
 {
-    init();
-    solve();
-    printf("%d", Ans);
     return 0;
 }
