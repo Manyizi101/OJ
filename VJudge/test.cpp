aa46cf2cbd13d8824336bf499e95663e37d5d6c6
Xuanwo <xuanwo.cn@gmail.com>
2015-08-16 01:20:28 +0800

    updated at 2015/08/16 周日  1:20:28.70

--- VJudge/test.cpp
+++ VJudge/test.cpp
@@ -1,46 +1,64 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <algorithm>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
 
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-string line;
-map<long long, int> base;
-int sum;
+const int maxn = 3 * 1e6 + 10;
 
-void dfs(int depth, int s , int e) {
-    if (line[s] == '[') {
-        int p = 0;
-        for (int i = s + 1; i != e; ++i) {
-            if (line[i] == '[') ++p;
-            if (line[i] == ']') --p;
-            if (p == 0 && line[i] == ',') {
-                dfs(depth + 1, s + 1, i - 1);
-                dfs(depth + 1, i + 1, e - 1);
-            }
+int d[maxn];
+int t[maxn];
+
+int main()
+{
+    int tt;
+    scanf("%d", &tt);
+    while (tt--) {
+        int n;
+        scanf("%d", &n);
+        int i;
+        for (i = 1; i <= n; ++i) {
+            scanf("%d", &d[i]);
         }
-    } else {
-        long long w = 0;
-        for (int i = s; i <= e; ++i) w = w * 10 + line[i] - '0';
-        ++sum, ++base[w << depth];
+        memset(t, 0, sizeof(t));
+        int ans = n;
+        for (i = 1; i <= n; ++i) {
+            if (t[d[i]] == 1) continue;
+            int start = i;
+            int x = i;
+            t[x] = 1;
+            while (1) {
+                x = d[x];
+                t[x] = 1;
+                if (x == start)
+                    break;
+            }
+            --ans;
+        }
+        printf("%d\n", ans + 1);
     }
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    int T;
-    cin >> T;
-    while (T--) {
-        cin >> line;
-        base.clear();
-        sum = 0;
-        dfs(0, 0, line.size() - 1);
-
-        int maxn = 0;
-        for (auto it = base.begin(); it != base.end(); ++it) maxn = max(maxn, it->second);
-
-        cout << sum - maxn << endl;
-    }
+    return 0;
 }
