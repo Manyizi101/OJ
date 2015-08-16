dbb8ff1c1bdf8ea9c9a4707be2795fccff534387
Xuanwo <xuanwo.cn@gmail.com>
2015-08-16 20:30:20 +0800

    updated at 2015/08/16 周日 20:30:20.38

---
+++ VJudge/88129B.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+using namespace std;
+
+const int maxn = 1000+10;
+int t,n;
+int ans;
+ll a[maxn];
+
+int main()
+{
+    scanf("%d", &t);
+    while(t--)
+    {
+        scanf("%d", &n);
+        for(int i=1; i<=n; ++i)    scanf("%I64d", &a[i]);
+        int tmp=0;
+        a[0]=0;
+        for(int i=1; i<=n; ++i)
+        {
+            if(a[i]-a[i-1]>0)
+            {
+                tmp++;
+            }
+            else
+            {
+                tmp=0;
+            }
+            ans=max(tmp,ans);
+        }
+        printf("%d\n", ans-1);
+    }
+}
+
