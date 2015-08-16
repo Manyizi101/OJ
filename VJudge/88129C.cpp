c0c74b11a352ce1c4e9efaf309bf34e885d022a5
Xuanwo <xuanwo.cn@gmail.com>
2015-08-17 02:00:59 +0800

    updated at 2015/08/17 周一  2:00:59.08

--- VJudge/88129C.cpp
+++ VJudge/88129C.cpp
@@ -27,6 +27,7 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+/*
 const int maxn = 65;
 const int N = 1010;
 ll a[N];
@@ -37,6 +38,15 @@ int rnk(int a[][maxn], int n, int m)
     int ret=0;
     for(int i=0, pivot = 0; i<n&&pivot<m; ++pivot)
     {
+        for(int x = 0; x<n; ++x)
+        {
+            for(int y = 0; y<m; ++y)
+            {
+                printf("%d ", a[x][y]);
+            }
+            printf("\n");
+        }
+        printf("\n");
         int pos = -1;
         for(int j=i; j<n; ++j)
         {
@@ -66,6 +76,7 @@ int rnk(int a[][maxn], int n, int m)
             }
         }
         i++;
+
     }
     return ret;
 }
@@ -96,8 +107,85 @@ int main()
                 b[i][j]=a[i]>>j&1;
             }
         }
-        if(rnk(b,n,maxn)<n) puts("Yes");
+        if(rnk(b,n,10)<n) puts("Yes");
         else    puts("No");
     }
 }
+**/
 
+
+const int maxn = 1010;
+const int maxm = 65;
+int t,n;
+ll a[maxn];
+int mat[maxm][maxm];
+
+int rnk(int mat[][maxm],int n, int m)
+{
+    int ret=0;
+    for(int i=0, it=0; i<n&&it<m; ++it)
+    {
+        int pos=-1;
+        for(int j=i; j<n; ++j)
+        {
+            if(mat[j][it])
+            {
+                pos=j;
+                break;
+            }
+        }
+        if(pos==-1)   continue;
+        ++ret;
+        if(pos!=i)
+        {
+            for(int j=it; j<m; ++j)
+            {
+                swap(mat[i][j],mat[pos][j]);
+            }
+        }
+        for(int j=0; j<n; ++j)
+        {
+            if(i!=j&&mat[j][pos])
+            {
+                for(int k=it; k<m; ++k)
+                {
+                    mat[j][k]^=mat[i][k];
+                }
+            }
+        }
+        ++i;
+    }
+    return ret;
+}
+
+int main()
+{
+    scanf("%d", &t);
+    while(t--)
+    {
+        ll sum=0;
+        memset(mat,0,sizeof(mat));
+        scanf("%d", &n);
+        for(int i=0; i<n; ++i)
+        {
+            scanf("%I64d", &a[i]);
+            sum^=a[i];
+        }
+        if(sum==0||n>maxm)
+        {
+            printf("Yes\n");
+            continue;
+        }
+        for(int i=0; i<n; ++i)
+        {
+            for(int j=0; j<maxm; ++j)
+            {
+                mat[i][j]=(a[i]>>j)&1;
+            }
+        }
+        int ret = rnk(mat,n,maxm);
+        if(ret<n)  printf("Yes\n");
+        else printf("No\n");
+    }
+    return 0;
+}
--- VJudge/test.cpp
+++ VJudge/test.cpp
@@ -27,38 +27,75 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-const int maxn = 3 * 1e6 + 10;
+const int maxn = 65;
+const int N = 1010;
+ll a[N];
+int b[maxn][maxn];
 
-int d[maxn];
-int t[maxn];
+int rnk(int a[][maxn], int n, int m)
+{
+    int ret = 0;
+    for (int i = 0, pivot = 0; i < n && pivot < m; ++pivot)
+    {
+        int pos = -1;
+        for (int j = i; j < n; ++j)
+        {
+            if (a[j][pivot] == 1)
+            {
+                pos = j;
+                break;
+            }
+        }
+        if (pos == -1)   continue;
+        if (pos != i)
+        {
+            for (int j = pivot; j < m; ++j)
+            {
+                swap(a[i][j], a[pos][j]);
+            }
+        }
+        ++ret;
+        for (int j = 0; j < n; ++j)
+        {
+            if (a[j][pivot] && j != i)
+            {
+                for (int k = pivot; k < m; ++k)
+                {
+                    a[j][k] ^= a[i][k];
+                }
+            }
+        }
+        i++;
+
+    }
+    return ret;
+}
+
 
 int main()
 {
-    int tt;
-    scanf("%d", &tt);
-    while (tt--) {
-        int n;
+    int t, n;
+    scanf("%d", &t);
+    while (t--)
+    {
+        ll sum = 0;
         scanf("%d", &n);
-        int i;
-        for (i = 1; i <= n; ++i) {
-            scanf("%d", &d[i]);
+        for (int i = 0; i < n; ++i)
+        {
+            scanf("%I64d", &a[i]);
+            sum ^= a[i];
         }
-        memset(t, 0, sizeof(t));
-        int ans = n;
-        for (i = 1; i <= n; ++i) {
-            if (t[d[i]] == 1) continue;
-            int start = i;
-            int x = i;
-            t[x] = 1;
-            while (1) {
-                x = d[x];
-                t[x] = 1;
-                if (x == start)
-                    break;
+        if (n > maxn || sum == 0)
+        {
+            puts("Yes");
+            continue;
+        }
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < maxn; ++j)
+            {
+                b[i][j] = a[i] >> j & 1;
             }
-            --ans;
-        }
-        printf("%d\n", ans + 1);
+        if (rnk(b, n, maxn) < n) puts("Yes");
+        else    puts("No");
     }
-    return 0;
 }
