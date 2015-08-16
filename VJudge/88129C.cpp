013f7056a7f8fbf0d73c427ddbd42dce1695e3e2
Xuanwo <xuanwo.cn@gmail.com>
2015-08-16 23:50:48 +0800

    updated at 2015/08/16 周日 23:50:48.87

--- VJudge/88129C.cpp
+++ VJudge/88129C.cpp
@@ -27,36 +27,10 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-const int maxn  = 1000+10;
-int t,n;
-ll a[maxn],ans[maxn];
-bool flag;
+
 
 int main()
 {
-    scanf("%d", &t);
-    while(t--)
-    {
-        flag=false;
-        scanf("%d",&n);
-        a[0]=0;
-        ans[0]=0;
-        for(int i=1; i<=n; ++i)
-        {
-            scanf("%I64d", &a[i]);
-            ans[i]=ans[i-1]^a[i];
-        }
-        if(n==1)
-        {
-            printf("NO\n");
-            continue;
-        }
-        for(int i=1;i<=n;++i)
-        {
-            if(ans[i]==0)   flag=true;
-        }
-        if(flag)    printf("YES\n");
-        else printf("NO\n");
-    }
+
 }
 
