9e635ec531894cd45b148db99c512d15dc739e25
Xuanwo <xuanwo.cn@gmail.com>
2015-08-20 16:33:35 +0800

    updated at 2015/08/20 周四 16:33:35.42

--- HOJ/Contests/test.cpp
+++ HOJ/Contests/test.cpp
@@ -1,108 +1,71 @@
-/*
-* this code is made by qcyclone
-* Problem: 1732
-* Verdict: Accepted
-* Submission Date: 2015-05-15 17:08:50
-* Time: 3988MS
-* Memory: 14240KB
-*/
-#include <iostream>
-#include <cstring>
-#include <string>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <cmath>
+#include <ctime>
+#include <iostream>
 #include <algorithm>
+#include <string>
 #include <vector>
-#include <queue>
+#include <deque>
+#include <list>
+#include <set>
 #include <map>
-#include<stack>
-#define inf 0x3f3f3f3f
-#define bug puts("bugbugbug");
-typedef long long ll;
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
-#define eps 1e-8
-#define MAXN 100000000
-char Is[MAXN>>3];
-struct add
+
+const ll mod = 1000000007;
+const int maxn  =  1e6+10;
+ll t,n;
+ll fac[maxn];
+
+ll quickpow(ll a, ll b)
 {
-    int val,co;
-    bool operator<(const add&u) const
+    if(b<0) return 0;
+    ll ret = 1;
+    a %= mod;
+    for(; b; b>>=1,a=(a*a)%mod)
+        if(b&1)
+            ret = (ret*a)%mod;
+    return ret;
+}
+
+ll inv(ll a)
+{
+    return quickpow(a,mod-2);
+}
+
+ll init()
+{
+    fac[1]=1;
+    for(ll i=2; i<=maxn; ++i)
     {
-        return val<u.val;
+        fac[i]=(fac[i-1]*(i*(inv(__gcd(fac[i-1],i))%mod))%mod)%mod;
     }
-}ad[11111];
-struct  node
-{
-    ll val,id,res;
-}Q[11111];
-bool cmp1(node a,node b)
-{
-    return  a.val<b.val;
 }
-bool cmp2(node a,node b)
+
+
+
+int main()
 {
-    return a.id<b.id;
-}
-void set_prime(int x)
-{
-    int t1 = x&7;
-    int t2 = x>>3;
-    Is[t2]|=(1<<t1);
-}
-int get_prime(int x)
-{
-    int t1 = x&7;
-    int t2 = x>>3;
-    int ret = Is[t2]&(1<<t1);
-    return ret==0;
-}
-int adcnt=0;
-void prime(int x)
-{
-    memset(Is,0,sizeof(Is));
-    set_prime(0);
-    set_prime(1);
-    for(int i=2;i*i<x;i++)
+    init();
+    scanf("%I64d", &t);
+    while(t--)
     {
-        if(!get_prime(i))   continue;  //不是质数
-        ll j=i*i;
-        while(j<MAXN)
-        {
-            set_prime(j);
-            j+=i;
-        }
-        ll t=i*i;
-        while(t<=MAXN)
-        {
-            ad[adcnt].val=t;
-            ad[adcnt++].co=i;
-            t=t*i;
-        }
+        scanf("%I64d", &n);
+        printf("%I64d\n", fac[n+1]*inv(n+1)%mod);
     }
-}
-int main(){
-    prime(MAXN);
-    sort(ad,ad+adcnt);
-    int T;
-    scanf("%d",&T);
-    for(int i=0;i<T;i++){
-        scanf("%d",&Q[i].val);
-        Q[i].id=i;
-    }
-    sort(Q,Q+T,cmp1);
-    int now=0,qi=0;
-    unsigned int ans=1;
-    for(int i=2;i<MAXN;i++){
-         if(get_prime(i))
-            ans=(ans*i);
-        if(now<adcnt && ad[now].val==i)
-            ans=(ans*ad[now++].co);
-        while(qi<T && Q[qi].val==i){
-            Q[qi++].res=ans;
-        }
-    }
-    sort(Q,Q+T,cmp2);
-    for(int i=0;i<T;i++)
-        printf("%u\n",Q[i].res);
-    return 0;
 }
