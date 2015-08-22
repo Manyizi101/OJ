fad8ed3ecf83948a33f53177916bb72b8bcbdb03
Xuanwo <xuanwo.cn@gmail.com>
2015-08-23 01:21:03 +0800

    updated at 2015/08/23 周日  1:21:03.89

--- CodeForces/572C.cpp
+++ CodeForces/572C.cpp
@@ -27,7 +27,63 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+struct cmps
+{
+    bool operator() (const int P1, const int P2)
+    {
+        return P1 > P2;
+    }
+};
+
+struct cmpb
+{
+    bool operator() (const int P1, const int P2)
+    {
+        return P1 < P2;
+    }
+};
+
+int n,s;
+char d;
+int p,q;
+
 int main()
 {
-
+    map<int, int, cmpb> mps;
+    map<int, int, cmps> mpb;
+    while(~scanf("%d%d", &n,&s))
+    {
+        mps.clear();
+        mpb.clear();
+        for(int i=0; i<n; ++i)
+        {
+            getchar();
+            scanf("%c %d %d", &d, &p, &q);
+            if(d=='S')  mps[p]+=q;
+            else if(d=='B') mpb[p]+=q;
+        }
+        auto it = mps.begin();
+        int tmp = s;
+        stack<pair<int ,int > > st;
+        while(it!=mps.end()&&tmp)
+        {
+            st.push(make_pair((*it).first,(*it).second));
+            tmp--;
+            ++it;
+        }
+        while(!st.empty())
+        {
+            auto x = st.top();
+            st.pop();
+            cout<<'S' <<" "<<x.first<<" "<<x.second<<endl;
+        }
+        it = mpb.begin();
+        tmp = s;
+        while(it!=mpb.end()&&tmp)
+        {
+            cout<<'B'<<" "<<(*it).first<<" "<<(*it).second<<endl;
+            tmp--;
+            ++it;
+        }
+    }
 }
