d6765cbfaadaf3de1dfa98cbf2063bb41640a613
Xuanwo <xuanwo.cn@gmail.com>
2015-08-23 20:10:29 +0800

    updated at 2015/08/23 周日 20:10:29.71

--- VJudge/89199C.cpp
+++ VJudge/89199C.cpp
@@ -27,36 +27,7 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-const int maxn = 100+10;
-
-int t,n;
-int tmp;
-
-struct node
-{
-    int x,id;
-}a[maxn];
-
-bool cmp(node a, node b)
-{
-    if(a.x==b.x)    return a.id<b.id;
-    return a.x>b.x;
-}
-
 int main()
 {
-    scanf("%d", &t);
-    while(t--)
-    {
-        scanf("%d", &n);
-        memset(a,0,sizeof(a));
-        for(int i=1;i<=100;++i) a[i].id=i;
-        for(int i=0; i<n; ++i)
-        {
-            scanf("%d", &tmp);
-            a[tmp].x++;
-        }
-        sort(a+1,a+110,cmp);
-        printf("%d\n",a[1].id);
-    }
+
 }
