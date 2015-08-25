8ca7b9903e61083cb3cf23333dfc5d454e03b408
Xuanwo <xuanwo.cn@gmail.com>
2015-08-25 22:13:01 +0800

    updated at 2015/08/25 周二 22:13:01.48

--- 51nod/1425.cpp
+++ 51nod/1425.cpp
@@ -9,29 +9,30 @@ using namespace std;
 typedef long long ll;
 const int N = 300005;
 map< pair<ll , ll> , pair<ll , ll> > f;
-pair<ll , ll> dfs(ll x , ll y) {
+pair<ll , ll> dfs(ll x , ll y)
+{
     if (x == 0)
         return make_pair(x || y , -y);
     if (x < 10)
         return make_pair(1ll , x - max(x , y));
     if (f.count({x , y}))
-        return f[{x , y}];
+        return f[ {x , y} ];
     ll ans = 0 , p = 1;
     while (p <= x / 10)
         p *= 10;
     pair<ll , ll> r = dfs(x % p , max(y , x / p));
     pair<ll , ll> b = dfs(x / p * p + r.second , y);
     ans = r.first + b.first;
-    return f[{x , y}] = make_pair(ans , b.second);
+    return f[ {x , y}] = make_pair(ans , b.second);
 }
 
-void work() {
-    ll n;
-    cin >> n;
-    cout << dfs(n , 0).first << endl;
-}
+ll n;
 
-int main() {
-    work();
+int main()
+{
+    while(cin>>n)
+    {
+        cout << dfs(n , 0).first << endl;
+    }
     return 0;
 }
