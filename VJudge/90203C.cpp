b16bb5f307bc3f04cecee28d6f7a72cf8698f795
Xuanwo <xuanwo.cn@gmail.com>
2015-08-31 15:59:41 +0800

    updated at 2015/08/31 周一 15:59:34.75

--- VJudge/90203C.cpp
+++ VJudge/90203C.cpp
@@ -27,39 +27,7 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-const int maxn = 1000+10;
-
-int n,a[maxn];
-int x[maxn];
-
-void init()
-{
-    memset(x,1,sizeof(x));
-    for(int i=2; i<=1000; ++i)
-    {
-        for(int j=1; j<i; ++j)
-        {
-            if(__gcd(i,j)>1)
-            {
-                x[i]=0;
-                break;
-            }
-        }
-    }
-    return;
-}
-
 int main()
 {
-    init();
-    while(~scanf("%d", &n))
-    {
-        int sum=0;
-        for(int i=0; i<n; ++i)
-        {
-            scanf("%d", &a[i]);
-            if(x[a[i]]) sum+=a[i];
-        }
-        printf("%d\n",sum);
-    }
+
 }
