87e29227fd396cb30fcabdfc4df40263f67c3fbf
Xuanwo <xuanwo.cn@gmail.com>
2015-09-01 15:38:41 +0800

    updated at 2015/09/01 周二 15:38:40.06

--- BNUOJ/6723I.cpp
+++ BNUOJ/6723I.cpp
@@ -19,6 +19,7 @@
 #include <sstream>
 #include <fstream>
 #define debug puts("-----")
+using namespace std;
 
 typedef long long int ll;
 const double pi = acos(-1.0);
@@ -26,91 +27,62 @@ const double eps = 1e-8;
 const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
-#define disc B*B-4*A*C//总判别式（discriminant=B*B-4*A*C）
-int sgn(double x);
-double pows(double x,double y)//VC++6.0的库函里面，只能给正数开方，不能给负数开奇次方。
-{
-    double a;
-    a=sgn(x)*pow(fabs(x),y);
-    return a;
-}
-int sgn(double x)//符号函数sgn x.x<0时,sgn x=-1;x=0时,sgn x=0;x>0时，sgn x=1.
-{
-    if(x>0)
-        return 1;
-    else if(x<0)
-        return -1;
-    else
-        return 0;
-}//上述两个函数解决了给负数开奇次方的问题。
+const double p=sqrt(3.0);
 
-double cal(double a,double b ,double c, double d)
+double aa,bb,cc,dd,ee,ff;
+double A,B,C,a,b,c,d;
+
+void gao(double a, double b, double c,double d)
 {
-    double A,B,C;
+    double ans;
     A=b*b-3*a*c;
     B=b*c-9*a*d;
     C=c*c-3*b*d;
-    double Y1,Y2;
-    Y1=A*b+3*a*(-B+sqrt(B*B-4*A*C))/2;
-    Y2=A*b+3*a*(-B-sqrt(B*B-4*A*C))/2;
-    double x1,x2,x3;
-    if(A==0&&B==0)
+    double q=B*B-4*A*C;
+    if(fabs(A-B)<eps&&fabs(A)<eps)
     {
-        x1 = -b/(3*a);
-        x2 = -c/b;
-        x3 = -3*d/c;
-        cout<<"x1="<<-b/(3*a)<<endl;
-        cout<<"x2="<<-c/b<<endl;
-        cout<<"x3="<<-3*d/c<<endl;
+        printf("%.8lf\n",sqrt(fabs(-c/b)));
+        return;
     }
-    else if(disc>0)
+    if(fabs(B*B-4*A*C)<eps)
     {
-        x1 = (-b-pows(Y1,1.0/3)-pows(Y2,1.0/3))/(3*a);
-        cout<<"x1="<<(-b-pows(Y1,1.0/3)-pows(Y2,1.0/3))/(3*a)<<endl;
-        cout<<"x2="<<(-2*b+pows(Y1,1.0/3)-pows(Y2,1.0/3))/(6*a)<<"+("<<(sqrt(3)*(pows(Y1,1.0/3)-pows(Y2,1.0/3)))/(6*a)<<")i"<<endl;
-        cout<<"x3="<<(-2*b+pows(Y1,1.0/3)+pows(Y2,1.0/3))/(6*a)<<"+("<<-(sqrt(3)*(pows(Y1,1.0/3)-pows(Y2,1.0/3)))/(6*a)<<")i"<<endl;
+        double k=B/A;
+        ans=min(fabs(-b/a+k),fabs(-k/2));
+        printf("%.8f\n",sqrt(ans));
+        return;
     }
-    else if(disc!=0)
+    if(q>0)
     {
-        double K;
-        K=B/A;
-        x1 = -b/a+K;
-        cout<<"x1="<<-b/a+K<<endl;
-        cout<<"x2="<<-K/2<<endl;
-        cout<<"x3="<<-K/2<<endl;
-    }
-    else if(disc<0)
-    {
-        double air,T;
-        T=(2*A*b-3*a*B)/pows(2*A,3.0/2);
-        air=acos(T);
-        if(A>0&&T<1.0&&T>-1.0)
-        {
-            x1 = (-b-sqrt(2*A)*cos(air/3))/(3*a);
-            cout<<"x1="<<(-b-sqrt(2*A)*cos(air/3))/(3*a)<<endl;
-            cout<<"x2="<<(-b-sqrt(A)*(cos(air/3)+pows(3,1.0/3)*sin(air/3)))/(3*a)<<endl;
-            cout<<"x3="<<(-b-sqrt(A)*(cos(air/3)-pows(3,1.0/3)*sin(air/3)))/(3*a);
-        }
-        else
-            cout<<"There is an error!";
+        double x=A*b+3*a*((-B+sqrt(q))/2);
+        double y=A*b+3*a*((-B-sqrt(q))/2);
+        double t1= x>0?pow(x,1.0/3):-(pow(fabs(x),1.0/3));
+        double t2= y>0?pow(y,1.0/3):-(pow(fabs(y),1.0/3));
+        ans=(-b-t1-t2)/(3*a);
+        printf("%.8f\n",sqrt(fabs(ans)));
+        return;
     }
     else
-        cout<<"There is an error!";
-    return x1;
+    {
+        double t=acos((2*A*b-3*a*B)/(2*sqrt(A*A*A)))/3;
+        double x1,x2,x3;
+        x1 = fabs((-b-2*sqrt(A)*cos(t))/(3*a));
+        x2 = fabs(((-b)+sqrt(A)*(cos(t)+p*sin(t)))/(3*a));
+        x3 = fabs(((-b)+sqrt(A)*(cos(t)-p*sin(t)))/(3*a));
+        ans = min(x1,min(x2,x3));
+        printf("%.8f\n",sqrt(ans));
+        return;
+    }
 }
-
-double aa,bb,cc,dd,ee,ff;
 
 int main()
 {
-    while(~scanf("%lf%lf%lf%lf%lf%lf", &aa, &bb,&cc,&dd,&ee,&ff))
+    while(~scanf("%lf%lf%lf%lf%lf%lf",&aa,&bb,&cc,&dd,&ee,&ff))
     {
-        double a,b,c,d;
-        dd/=2,ee/=2,ff/=2;
-        a = 1.0;
-        b = -(aa+bb+cc);
-        c = aa*bb+bb*cc+cc*aa-dd*dd-ee*ee-ff*ff;
-        d = -(aa*bb*cc+2*dd*ee*ff-aa*ee*ee-bb*ff*ff-cc*dd*dd);
-        double ans = cal(a,b,c,d);
+        a=4*aa*bb*cc+dd*ee*ff-bb*ee*ee-aa*dd*dd-ff*ff*cc;
+        b=4*aa*bb+4*bb*cc+4*aa*cc-ee*ee-ff*ff-dd*dd;
+        c=4*aa+4*bb+4*cc;
+        d=4.0;
+        gao(a,b,c,d);
     }
+    return 0;
 }
