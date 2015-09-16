de63613add52ad775642f5d1b3170730fe037c60
Xuanwo <xuanwo.cn@gmail.com>
2015-09-16 21:12:31 +0800

    updated at 2015/09/16 周三 21:12:31.06

--- VJudge/91782A.cpp
+++ VJudge/91782A.cpp
@@ -27,7 +27,30 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+const int maxn = 100000+10;
+
+int t;
+ll n;
+ll x[maxn];
+ll k;
+
 int main()
 {
-
+    scanf("%d", &t);
+    while(t--)
+    {
+        scanf("%I64d%I64d", &n, &k);
+        for(ll i=1; i<=n; ++i)
+        {
+            scanf("%I64d", &x[i]);
+        }
+        sort(x+1,x+n+1);
+        ll sum=0;
+        for(int i=1;i<=n;++i)
+        {
+            int pos = upper_bound(x+i,x+n+1,x[i]+k)-x-i;
+            sum += n - max(n-pos,(ll)0);
+        }
+        printf("%I64d\n", sum - n);
+    }
 }
