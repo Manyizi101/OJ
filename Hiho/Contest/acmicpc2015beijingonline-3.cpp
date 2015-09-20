52cbcdbdb10745f8fdf41ab11bd6a3d5e557520e
Xuanwo <xuanwo.cn@gmail.com>
2015-09-20 15:37:05 +0800

    updated at 2015/09/20 周日 15:37:05.60

--- Hiho/Contest/acmicpc2015beijingonline-3.cpp
+++ Hiho/Contest/acmicpc2015beijingonline-3.cpp
@@ -1,74 +1,144 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <ctime>
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <deque>
-#include <list>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <numeric>
-#include <iomanip>
-#include <bitset>
-#include <sstream>
-#include <fstream>
-#define debug puts("-----")
-
-typedef long long int ll;
-const double pi = acos(-1.0);
-const double eps = 1e-8;
-const int inf = 0x3f3f3f3f;
-const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<ostream>
+#include<istream>
+#include<algorithm>
+#include<queue>
+#include<string>
+#include<cmath>
+#include<set>
+#include<map>
+#include<stack>
+#include<vector>
+#define fi first
+#define se second
+#define ll long long
+#define pii pair<int,int>
+#define inf (1<<30)
+#define eps 1e-8
+#define pb push_back
 using namespace std;
-
-int cal(int arr[],int n)
+const int maxn=110005;
+struct Node
 {
-    int sum = 0;
-    for(int i = 0; i < n; ++i)
-    {
-        for(int j = i+1; j < n; ++j)
-        {
-            if(arr[i] > arr[j])
-            {
-                ++sum;
+    int d[8];
+    int step;
+};
+bool ok(Node& u,int p,int a,int w)
+{
+    for(int i=0;i<a;i++) {
+        if(u.d[i]==p)
+            return false;
+    }
+    return true;
+}
+bool vis[maxn*10];
+int ans[8][1000000+5];
+int Num(Node& u,int w)
+{
+    int ans=0;
+    for(int i=0;i<w;i++) {
+        ans=ans*w+u.d[i];
+    }
+    return ans;
+}
+int Min(Node& u,int p,int w)
+{
+    int me=10;
+    for(int i=0;i<w;i++) {
+        if(u.d[i]==p)
+            me=min(me,i);
+    }
+    return me;
+}
+void gao(int w)
+{
+    memset(vis,0,sizeof(vis));
+    Node u;
+    for(int i=0;i<w;i++)
+        u.d[i]=i;
+    u.step=0;
+    queue<Node>que;
+    que.push(u);
+    while(!que.empty()) {
+        Node v=que.front();
+        que.pop();
+        int aa=Num(v,w);
+        if(vis[aa])
+            continue;
+        ans[w][aa]=v.step;
+        vis[aa]=1;
+        for(int i=0;i<w;i++) {
+            int pos=v.d[i];
+            if(!ok(v,pos,i,w))
+                continue;
+            if(pos-1>=0) {
+                int a=Min(v,pos-1,w);
+                if(a>i) {
+                    Node p;
+                    for(int j=0;j<w;j++)
+                        p.d[j]=v.d[j];
+                    p.d[i]=pos-1;
+                    p.step=v.step+1;
+                    if(vis[Num(p,w)]==0)
+                        que.push(p);
+                }
+            }
+            if(pos+1<w) {
+                int a=Min(v,pos+1,w);
+                if(a>i) {
+                    Node p;
+                    for(int j=0;j<w;j++)
+                        p.d[j]=v.d[j];
+                    p.d[i]=pos+1;
+                    p.step=v.step+1;
+                    if(vis[Num(p,w)]==0)
+                        que.push(p);
+                }
             }
         }
     }
-    return sum;
 }
-
-const int maxn = 100;
-
-int t,n,v[maxn];
-
 int main()
 {
-    scanf("%d", &t);
-    while(t--)
-    {
-        scanf("%d", &n);
-        for(int i=0; i<n; ++i)    scanf("%d", &v[i]);
-        int cnt = cal(v,n);
-        if(cnt==0)
-        {
-            printf("0\n");
-        }
-        else
-        {
-            if(n==2)
-            {
-                printf("-1\n");
-            }
-            else
-            {
-                printf("%d\n", 4*(2*cnt-1));
-            }
-        }
+    memset(ans,-1,sizeof(ans));
+    int a;
+    for(int i=1;i<=7;i++) {
+        gao(i);
+        //cout<<1<<endl;
     }
+    int t;
+    int n;
+    int d[10];
+    int dis[10];
+    scanf("%d",&t);
+    while(t--) {
+        scanf("%d",&n);
+        if(n<=0) {
+            printf("0\n");
+            continue;
+        }
+        for(int i=0;i<n;i++) {
+            scanf("%d",&d[i]);
+            dis[i]=d[i];
+        }
+        sort(dis,dis+n);
+        int u=0;
+        for(int i=0;i<n;i++) {
+            d[i]=lower_bound(dis,dis+n,d[i])-dis;
+        }
+        int pp[10];
+        for(int i=0;i<n;i++) {
+            pp[d[i]]=i;
+        }
+        for(int i=0;i<n;i++) {
+            u=u*n+pp[i];
+        }
+        printf("%d\n",ans[n][u]);
+    }
+    return 0;
 }
+
+
+
