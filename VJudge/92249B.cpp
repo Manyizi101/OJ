7ca11873fa03b4bad2baa7b8f6667b41ee0d1bf1
Xuanwo <xuanwo.cn@gmail.com>
2015-09-21 21:10:29 +0800

    updated at 2015/09/21 周一 21:10:29.24

--- VJudge/92249B.cpp
+++ VJudge/92249B.cpp
@@ -27,7 +27,34 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+ll n,p;
+
+ll mult_mod(ll a,ll b,ll mod) //(a*b)%c a,b,c<2^63
+{
+    return (a*b-(ll)(a/(long double)mod*b+1e-3)*mod+mod)%mod;
+}
+
+ll qPow(ll a, ll b,ll mod)
+{
+    if(b<0) return 0;
+    ll ret = 1;
+    a %= mod;
+    for(; b; b>>=1,a=(mult_mod(a,a,p))%mod)
+        if(b&1)
+            ret = (mult_mod(ret,a,p))%mod;
+    return ret;
+}
+
 int main()
 {
-
+    while(~scanf("%I64d%I64d", &n, &p))
+    {
+        if(n==1)    printf("%I64d\n", 1%p);
+        else
+        {
+            ll ans = qPow(2,n,p)-2;
+            printf("%I64d\n", (ans+p)%p);
+        }
+    }
+    return 0;
 }
