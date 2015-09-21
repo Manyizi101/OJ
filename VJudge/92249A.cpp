f74c87771aad398031ebb3dc97b1094ed48b6c03
Xuanwo <xuanwo.cn@gmail.com>
2015-09-21 20:33:01 +0800

    updated at 2015/09/21 周一 20:33:01.81

--- VJudge/92249A.cpp
+++ VJudge/92249A.cpp
@@ -27,7 +27,58 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+const int maxn = 1000+10;
+
+int n,B;
+char a[maxn][1010];
+int b[maxn];
+
 int main()
 {
+    while(~scanf("%d%d", &n,&B))
+    {
+        int len=0;
+        for(int i=0; i<n; ++i)
+        {
+            scanf("%s", a[i]);
+            char tmp[1010];
+            int tlen = strlen(a[i]);
+            for(int j=tlen-1; j>=0; --j)
+            {
+                tmp[tlen-j-1]=a[i][j];
+            }
+            for(int j=0; j<tlen; ++j)
+            {
+                a[i][j]=tmp[j];
+            }
+            len=max(len,(int)strlen(a[i]));
+        }
+        memset(b,0,sizeof(b));
+        for(int i=0; i<len; ++i)
+        {
+            b[i]=0;
+            for(int j=0; j<n; ++j)
+            {
+                if(i>=(int)strlen(a[j]))  continue;
+                if(a[j][i]>='0'&&a[j][i]<='9')
+                {
+                    b[i]+=a[j][i]-'0';
+                }
+                else if(a[j][i]>='a'&&a[j][i]<='z')
+                {
+                    b[i]+=a[j][i]-'a'+10;
+                }
+            }
+        }
+        int flag=0;
+        for(flag=len-1; flag>=0; --flag)
+        {
+            if(b[flag]%B!=0)   break;
+        }
+        if(b[flag]==0) printf("0");
+        else
+            for(; flag>=0; --flag)    printf("%d", b[flag]%B);
+        printf("\n");
+    }
 
 }
