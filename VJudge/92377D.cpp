8f7474f3aadf0bcc8ff2d014d7fc3785e0c125d9
Xuanwo <xuanwo.cn@gmail.com>
2015-09-22 20:27:52 +0800

    updated at 2015/09/22 周二 20:27:52.80

--- VJudge/92377D.cpp
+++ VJudge/92377D.cpp
@@ -22,131 +22,128 @@
 
 typedef long long int ll;
 const double pi = acos(-1.0);
-const double eps = 1e-8;
 const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
-struct node
-{
-    double x,y;
-    node(){}
-    node(double xx,double yy)
-    {
-        x=xx;
-        y=yy;
-    }
-    node operator -(node s)
-    {
-        return node(x-s.x,y-s.y);
-    }
-    node operator +(node s)
-    {
-        return node(x+s.x,y+s.y);
-    }
-    double operator *(node s)
-    {
-        return x*s.x+y*s.y;
-    }
-    double operator ^(node s)
-    {
-        return x*s.y-y*s.x;
-    }
-}p[M];
-double max(double a,double b)
-{
-    return a>b?a:b;
-}
-double min(double a,double b)
-{
-    return a<b?a:b;
-}
-double len(node a)
-{
-    return sqrt(a*a);
-}
-double dis(node a,node b)//两点之间的距离
-{
-    return len(b-a);
-}
-double cross(node a,node b,node c)//叉乘
-{
-    return (b-a)^(c-a);
-}
-double dot(node a,node b,node c)//点成
-{
-    return (b-a)*(c-a);
-}
-int judge(node a,node b,node c)//判断c是否在ab线段上（前提是c在直线ab上）
-{
-    if(c.x>=min(a.x,b.x)
-       &&c.x<=max(a.x,b.x)
-       &&c.y>=min(a.y,b.y)
-       &&c.y<=max(a.y,b.y))
-        return 1;
-    return 0;
-}
-double area(node b,node c,double r)
-{
-    node a(0.0,0.0);
-    if(dis(b,c)<eps)
-        return 0.0;
-    double h=fabs(cross(a,b,c))/dis(b,c);
-    if(dis(a,b)>r-eps&&dis(a,c)>r-eps)//两个端点都在圆的外面则分为两种情况
-    {
-        double angle=acos(dot(a,b,c)/dis(a,b)/dis(a,c));
-        if(h>r-eps)
-        {
-            return 0.5*r*r*angle;
-        }
-        else if(dot(b,a,c)>0&&dot(c,a,b)>0)
-        {
-            double angle1=2*acos(h/r);
-            return 0.5*r*r*fabs(angle-angle1)+0.5*r*r*sin(angle1);
-        }
-        else
-        {
-            return 0.5*r*r*angle;
-        }
-    }
-    else if(dis(a,b)<r+eps&&dis(a,c)<r+eps)//两个端点都在圆内的情况
-    {
-        return 0.5*fabs(cross(a,b,c));
-    }
-    else//一个端点在圆上一个端点在圆内的情况
-    {
-        if(dis(a,b)>dis(a,c))//默认b在圆内
-        {
-            swap(b,c);
-        }
-        if(fabs(dis(a,b))<eps)//ab距离为0直接返回0
-        {
-            return 0.0;
-        }
-        if(dot(b,a,c)<eps)
-        {
-            double angle1=acos(h/dis(a,b));
-            double angle2=acos(h/r)-angle1;
-            double angle3=acos(h/dis(a,c))-acos(h/r);
-            return 0.5*dis(a,b)*r*sin(angle2)+0.5*r*r*angle3;
+#define sqr(v) ((v)*(v))
 
-        }
-        else
-        {
-            double angle1=acos(h/dis(a,b));
-            double angle2=acos(h/r);
-            double angle3=acos(h/dis(a,c))-angle2;
-            return 0.5*r*dis(a,b)*sin(angle1+angle2)+0.5*r*r*angle3;
+const double eps = 1e-6;
+
+double my_sqrt(double d) { return sqrt(max(d, 0.0)); }
+double my_acos(double d) { return acos(d > 1 ? 1 : d < -1 ? -1 : d); }
+
+int sig(double d){
+    return (d > eps) - (d < -eps);
+}
+
+struct Point{
+    double x, y;
+    Point() {}
+    Point(double x, double y) : x(x), y(y) {}
+    Point resize(double d){
+        d /= my_sqrt(x*x + y*y);
+        return Point(x*d, y*d);
+    }
+    Point left90(){
+        return Point(-y, x);
+    }
+    Point operator - (const Point & o) const{
+        return Point(this -> x - o.x, this -> y - o.y);
+    }
+    Point operator + (const Point & o) const{
+        return Point(this -> x + o.x, this -> y + o.y);
+    }
+    Point operator * (double d) const{
+        return Point(x * d, y * d);
+    }
+};
+
+double cross(Point o, Point a, Point b){
+    return (a.x - o.x)*(b.y - o.y) - (b.x - o.x)*(a.y - o.y);
+}
+
+double dot(Point o, Point a, Point b){
+    return (a.x - o.x)*(b.x - o.x) + (a.y - o.y)*(b.y - o.y);
+}
+
+double dis(Point a, Point b){
+    return my_sqrt(sqr(a.x - b.x) + sqr(a.y - b.y));
+}
+
+double btw(Point o, Point a, Point b){
+    return sig(dot(o, a, b));
+}
+
+struct Circle{
+    Point center;
+    double radis;
+};
+
+double pointToLine(Point o, Point a, Point b, Point & res){
+    double d = dis(a, b);
+    double s = cross(a, b, o) / d;
+    res = o + (a- b).left90()*(s/d);
+    return fabs(s);
+}
+
+int intersect(Point a, Point b, Circle c, Point &p1, Point &p2){
+    Point p;
+    double d = pointToLine(c.center, a, b, p);
+    int v = sig(c.radis - d);
+    if(v != -1){
+        Point vec = (b - a).resize(my_sqrt(sqr(c.radis) - sqr(d)));
+        p1 = p + vec;
+        p2 = p - vec;
+    }
+    return v;
+}
+
+//圆与三角形的相交面积
+double intersectArea(Circle c, Point a, Point b){
+    if(sig(cross(c.center, a, b)) == 0) return 0.0;
+
+    Point o = c.center, p[5];
+    double r = c.radis;
+    int len = 0;
+    p[len++] = a;
+    if(intersect(a, b, c, p[1], p[2]) == 1){
+        if(btw(p[1], a, b) < 0) p[len++] = p[1];
+        if(btw(p[2], a, b) < 0) p[len++] = p[2];
+    }
+    p[len++] = b;
+    if(len == 4 && btw(p[1], p[0], p[2]) > 0) swap(p[1], p[2]);
+
+    double res = 0;
+    for(int i = 0; i < len-1; i++){
+        if(sig(dis(o, p[i]) - r) > 0 || sig(dis(o, p[i+1]) - r) > 0){
+            double theta = my_acos(dot(o, p[i], p[i+1])/dis(o, p[i])/dis(o, p[i+1]));
+            res += theta * r * r / 2.0;
+        }else{
+            res += fabs(cross(o, p[i], p[i+1])/2.0);
         }
     }
+    if(sig(cross(o, a, b)) < 0) res = - res;
+    return res;
 }
+
+double intersectArea(Circle c, Point *ps, int n){
+    ps[n] = ps[0];
+    double res = 0;
+    for(int i = 0; i < n; i++){
+        res += intersectArea(c, ps[i], ps[i+1]);
+    }
+    return fabs(res);
+}
+
+const int maxn = 510;
 
 int n;
 double k;
-
+Point p[maxn];
+int cs = 0;
 int main()
 {
-    double x,y,h,x1,y1,R;
     double x1,x2,y1,y2;
     while(~scanf("%d%lf", &n,&k))
     {
@@ -154,23 +151,20 @@ int main()
         {
             scanf("%lf%lf",&p[i].x,&p[i].y);
         }
-        p[n]=p[0];
-        scanf("%lf%lf%lf%lf",&x1,&y1,&x2,&y2);
-        node O(x0,y0);
-        for(int i=0;i<=n;i++)
-            p[i]=p[i]-O;
-        O=node(0,0);
-        double sum=0;
-        for(int i=0;i<n;i++)
-        {
-            int j=i+1;
-            double s=area(p[i],p[j],R);
-            if(cross(O,p[i],p[j])>0)
-                sum+=s;
-            else
-                sum-=s;
-        }
-        printf("%.2lf\n",fabs(sum));
+        scanf("%lf%lf%lf%lf", &x1,&y1,&x2,&y2);
+        double a = 1-k*k;
+        double c = (2*x1*k*k-2*x2)/a;
+        double d = (2*y1*k*k-2*y2)/a;
+        double e = (x2*x2-k*k*x1*x1+y2*y2-k*k*y1*y1)/a;
+        double xx = -c/2;
+        double yy = -d/2;
+        double r = sqrt(-e+c*c/4+d*d/4);
+        Circle C;
+        C.center.x = xx;
+        C.center.y = yy;
+        C.radis = r;
+        printf("Case %d: ", ++cs);
+        printf("%.10f\n", intersectArea(C,p,n));
     }
     return 0;
 }
