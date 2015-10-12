838d3897e9abe94515db5bed8237f5fbf72afcd3
Xuanwo <xuanwo.cn@gmail.com>
2015-10-12 20:06:49 +0800

    updated at 10/12/2015 Mon 20:06:48.84

--- VJudge/94921A.cpp
+++ VJudge/94921A.cpp
@@ -27,7 +27,25 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+const int maxn = 40;
+
+int n;
+int a[maxn][maxn];
+
 int main()
 {
-
+    while(~scanf("%d", &n))
+    {
+        int ans =0;
+        memset(a,0,sizeof(a));
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                scanf("%d", &a[i][j]);
+                if(a[i][j]==1)  ans++;
+            }
+        }
+        printf("%d\n", ans+2*n);
+    }
 }
