2202e9a07a0dbf0bbfde1c9915a532d33686cb3f
Xuanwo <xuanwo.cn@gmail.com>
2015-10-20 21:57:49 +0800

    updated at 10/20/2015 Tue 21:57:49.82

---
+++ CodeForces/546A.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+using namespace std;
+
+int k,n,w;
+
+int main()
+{
+    while(~scanf("%d%d%d", &k, &n, &w)) {
+        cout<<max(w*(w+1)/2*k-n,0)<<endl;
+    }
+}
