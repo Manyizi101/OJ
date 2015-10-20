1cbe807d2a5d97cfac9bc4434b4cc0d436715f06
Xuanwo <xuanwo.cn@gmail.com>
2015-10-20 18:57:13 +0800

    updated at 10/20/2015 Tue 18:56:36.20

---
+++ CodeForces/10A.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+using namespace std;
+
+const int maxn = 100+10;
+
+int n,p1,p2,p3,t1,t2;
+int l[maxn],r[maxn];
+
+int main()
+{
+
+}
