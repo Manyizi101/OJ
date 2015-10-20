784bfdec42fb24f6c7f2f9f4fdb43b15148de010
Xuanwo <xuanwo.cn@gmail.com>
2015-10-20 22:00:20 +0800

    updated at 10/20/2015 Tue 22:00:20.31

---
+++ CodeForces/486A.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <fstream>
+#define debug puts("-----")
+
+typedef long long int ll;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 0x3f3f3f3f;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+using namespace std;
+
+ll n;
+
+int main()
+{
+    while(~scanf("%I64d", &n)) {
+        if(n&1) cout<<-(n+1)/2<<endl;
+        else cout<<n/2<<endl;
+    }
+}
