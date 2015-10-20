649128244dac6080ab44c98bb1fe53053248b204
Xuanwo <xuanwo.cn@gmail.com>
2015-10-20 19:51:02 +0800

    updated at 10/20/2015 Tue 19:51:02.13

--- CodeForces/148A.cpp
+++ CodeForces/148A.cpp
@@ -27,7 +27,32 @@ const int inf = 0x3f3f3f3f;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 using namespace std;
 
+const int maxn = 1e5+10;
+int k,l,m,n,d,ans;
+int vis[maxn];
+
 int main()
 {
-
+    while(~scanf("%d%d%d%d%d", &k, &l, &m, &n, &d)) {
+        memset(vis,0,sizeof(vis));
+        ans = 0;
+        for(int i=0; i<=d; i+=k) {
+            vis[i]=1;
+        }
+        for(int i=0; i<=d; i+=l) {
+            vis[i]=1;
+        }
+        for(int i=0; i<=d; i+=m) {
+            vis[i]=1;
+        }
+        for(int i=0; i<=d; i+=n) {
+            vis[i]=1;
+        }
+        vis[0]=0;
+        for(int i=0; i<=d; ++i) {
+            if(vis[i]) ans++;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
 }
