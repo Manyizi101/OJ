bfaa33c3f63a9b7e1359d051a1e9afe927939d85
Xuanwo <xuanwo.cn@gmail.com>
2016-05-29 10:34:18 +0800

    solve the CF 500A

--- CodeForces/500A.py
+++ CodeForces/500A.py
@@ -1,15 +1,11 @@
 n ,t = list(map(int, input().split(' ')))
 a = list(map(int, input().split(' ')))
-b = [a[i]+i+1 for i in range(n-1)]
+q = 1
 
-def dfs(i):
-    if i == t:
-        return True
-    if i > t or i > n - 1 or b[i-1] > n or b[i-1] > t:
-        return False
-    return dfs(b[i-1])
+while q<t:
+    q = q + a[q-1]
 
-if dfs(1):
+if q == t:
     print("YES")
 else:
     print("NO")
