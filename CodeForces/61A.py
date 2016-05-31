5bb858c9a51959b6b12ce452080f7fce2477bc75
Xuanwo <xuanwo.cn@gmail.com>
2016-05-31 23:37:47 +0800

    the better way to solve the CF 61A

--- CodeForces/61A.py
+++ CodeForces/61A.py
@@ -1,9 +1,2 @@
-a = input()
-b = input()
-c = []
-for i,j in zip(a,b):
-    if i==j:
-        c.append('0')
-    else:
-        c.append('1')
-print(''.join(c))
+a,b = input(), input()
+print(''.join('01'[i!=j] for i,j in zip(a,b)))
